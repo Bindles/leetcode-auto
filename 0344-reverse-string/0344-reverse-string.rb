559eade55766a7a20949d92a8a3681e717314c0d
Nicholas Turner <bindlez@gmail.com>
2024-05-10 18:55:07 -0400

    Time: 187 ms (57.14%), Space: 225.9 MB (85%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.rb
@@ -0,0 +1,10 @@
+# @param {Character[]} s
+# @return {Void} Do not return anything, modify s in-place instead.
+def reverse_string(s)
+  left, right = 0, s.size-1
+  while left < right
+    s[left], s[right] = s[right], s[left]
+    left += 1
+    right -=1
+  end
+end
