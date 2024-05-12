65cf8c6b0bd094c16162d85bb5e9af9ebc3b689e
Nicholas Turner <bindlez@gmail.com>
2024-05-12 04:37:44 -0400

    Time: 1 ms (76.5%), Space: 2.2 MB (58.5%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.rs
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.rs
@@ -1,12 +1,8 @@
 impl Solution {
   pub fn search(nums: Vec<i32>, target: i32) -> i32 {
-      let mut nums_with_indices: Vec<(i32, usize)> = nums.iter().enumerate().map(|(i, &val)| (val, i)).collect();
-      nums_with_indices.sort_unstable_by_key(|&(val, _)| val);
-
-      let result = nums_with_indices.binary_search_by_key(&target, |&(val, _)| val);
-
-      match result {
-          Ok(index) => nums_with_indices[index].1 as i32,
+      let f = |x: &i32| (*x < nums[0], *x);
+      match nums.binary_search_by_key(&f(&target), f) {
+          Ok(i) => i as i32,
           Err(_) => -1,
       }
   }
