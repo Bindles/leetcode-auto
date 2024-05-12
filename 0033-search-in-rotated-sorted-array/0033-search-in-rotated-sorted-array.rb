6aa33875b5bd12d0c5d28ea3d918cbd551a018dc
Nicholas Turner <bindlez@gmail.com>
2024-05-12 04:39:09 -0400

    Time: 76 ms (6.58%), Space: 211.7 MB (5.26%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.rb
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.rb
@@ -1,12 +1,4 @@
-# @param {Integer[]} nums
-# @param {Integer} target
-# @return {Integer}
 def search(nums, target)
-  spaceship_operator_head_basher = ->(target, n) do
-    return 0 if target == n
-    return -1 if target < n
-    return 1 if target > n
-  end
-  result = nums.sort.bsearch { |n| spaceship_operator_head_basher[target, n] }
+  result = nums.sort.bsearch{ |n| target <=> n }
   result.nil? ? -1 : nums.index(result)
 end
