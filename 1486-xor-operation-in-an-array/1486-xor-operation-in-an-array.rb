b17a40eaa5ef24572bc1bcebb3b760114689b15f
Nicholas Turner <bindlez@gmail.com>
2024-05-12 17:56:50 -0400

    Time: 65 ms (50%), Space: 211.3 MB (100%) - LeetHub

--- 1486-xor-operation-in-an-array/1486-xor-operation-in-an-array.rb
+++ 1486-xor-operation-in-an-array/1486-xor-operation-in-an-array.rb
@@ -2,6 +2,5 @@
 # @param {Integer} start
 # @return {Integer}
 def xor_operation(n, start)
-    p arr = Array.new(n, 0)
-    arr.map.with_index{|num,index| start + num + 2 * index}.reduce(&:^)
+    Array.new(n, 0).map.with_index{|num,index| start + num + 2 * index}.reduce(&:^)
 end
