5e6d84bff6df59c15d6009dccaabaa190ed05d75
Nicholas Turner <bindlez@gmail.com>
2024-05-12 00:37:46 -0400

    Time: 64 ms (50.82%), Space: 212 MB (42.62%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.rb
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.rb
@@ -2,9 +2,6 @@
 # @param {Integer} target
 # @return {Integer[]}
 def search_range(nums, target)
-    res=[]
-    nums.each_with_index do |n,i|
-        res << i if n == target
-    end
+    res = nums.each_with_index.filter_map{|n,i| i if n == target}
     res.empty? ? [-1, -1] : [res.first, res.last]
 end
