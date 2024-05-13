998a0f3067424f46fa36b992c510e94078d82844
Nicholas Turner <bindlez@gmail.com>
2024-05-13 17:55:19 -0400

    Time: 437 ms (84.62%), Space: 240.3 MB (69.23%) - LeetHub

--- 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.rb
+++ 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.rb
@@ -1,5 +1,17 @@
 # @param {Integer[]} nums
 # @return {Integer[]}
 def rearrange_array(nums)
-  nums.partition(&:positive?).transpose.flatten
+  result = Array.new(nums.size, 0)
+
+  index_even, index_odd = 0, 1
+  nums.each do |num|
+    if num > 0
+      result[index_even] = num
+      index_even += 2
+    else
+      result[index_odd] = num
+      index_odd += 2
+    end
+  end
+  result
 end
