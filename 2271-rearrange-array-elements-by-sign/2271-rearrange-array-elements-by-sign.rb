754a08d49544a4a1bdc17d7e9dd5e1a3e41b6bd0
Nicholas Turner <bindlez@gmail.com>
2024-05-13 17:57:01 -0400

    Time: 481 ms (46.15%), Space: 240.5 MB (61.54%) - LeetHub

--- 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.rb
+++ 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.rb
@@ -1,17 +1,38 @@
 # @param {Integer[]} nums
 # @return {Integer[]}
 def rearrange_array(nums)
-  result = Array.new(nums.size, 0)
-
-  index_even, index_odd = 0, 1
-  nums.each do |num|
-    if num > 0
-      result[index_even] = num
-      index_even += 2
-    else
-      result[index_odd] = num
-      index_odd += 2
+  # Initialize left and right pointers to track positive and negative elements
+  l = r = 0
+  # Initialize an empty array to store the rearranged elements
+  res = []
+  # Initialize an index to track the current position in the original array
+  i = 0
+  
+  # Iterate through the original array
+  while i < nums.size
+    # If the index is even
+    if i.even?
+      # Move the right pointer until finding the next positive number
+      while r < nums.size && nums[r] < 0
+        r += 1
+      end
+      # Append the positive number to the result array
+      res << nums[r]
+      # Move the right pointer to the next position
+      r += 1
+    else # If the index is odd
+      # Move the left pointer until finding the next negative number
+      while l < nums.size && nums[l] > 0
+        l += 1
+      end
+      # Append the negative number to the result array
+      res << nums[l]
+      # Move the left pointer to the next position
+      l += 1
     end
+    # Move to the next index
+    i += 1
   end
-  result
+  # Return the rearranged array
+  res
 end
