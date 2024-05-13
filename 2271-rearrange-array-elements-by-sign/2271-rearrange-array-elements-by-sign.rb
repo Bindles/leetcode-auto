485fa875a7d3192bd2ee4be0bb48fb169cdc5244
Nicholas Turner <bindlez@gmail.com>
2024-05-13 17:54:44 -0400

    Time: 551 ms (7.69%), Space: 255.6 MB (7.69%) - LeetHub

--- 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.rb
+++ 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.rb
@@ -1,6 +1,5 @@
 # @param {Integer[]} nums
 # @return {Integer[]}
 def rearrange_array(nums)
-  positives, negatives = nums.partition(&:positive?)
-  positives.zip(negatives).flatten
+  nums.partition(&:positive?).transpose.flatten
 end
