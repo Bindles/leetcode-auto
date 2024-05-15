72e5915d1308cdfc0e214ff945d347f663e3e7df
Nicholas Turner <bindlez@gmail.com>
2024-05-15 11:58:53 -0400

    Time: 102 ms (33.33%), Space: 211.5 MB (100%) - LeetHub

--- 0821-shortest-distance-to-a-character/0821-shortest-distance-to-a-character.rb
+++ 0821-shortest-distance-to-a-character/0821-shortest-distance-to-a-character.rb
@@ -3,9 +3,9 @@
 # @return {Integer[]}
 def shortest_to_char(s, c)
   res=[]
-  nums = s.each_char.with_index.filter_map {|char, i| i if char == c}
-  s.chars.each_index do |num|
-    res << (num -  nums.min_by {|x| (x - num).abs}).abs
+  char_indices = s.each_char.with_index.filter_map {|char, i| i if char == c}
+  s.chars.each_index do |index|
+    res << (index - char_indices.min_by {|x| (x - index).abs}).abs
   end
   res
 end
