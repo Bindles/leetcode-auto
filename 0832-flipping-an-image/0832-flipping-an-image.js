5836b4d982ccc998fb345209a16195cef3b03dad
Nicholas Turner <bindlez@gmail.com>
2024-05-17 00:00:41 -0400

    Time: 61 ms (51.11%), Space: 52 MB (20.26%) - LeetHub

--- 0832-flipping-an-image/0832-flipping-an-image.js
+++ 0832-flipping-an-image/0832-flipping-an-image.js
@@ -4,7 +4,5 @@
  */
 var flipAndInvertImage = function(image) {
   const reversed_image = image.map(row => row.reverse());
-  return reversed_image.map(sub => {
-    return sub.map(num => num === 0 ? 1 : 0)
-  })
-}
+  return reversed_image.map(sub => sub.map(num => 1 - num));
+};
