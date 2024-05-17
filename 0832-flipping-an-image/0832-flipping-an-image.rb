13a4b3b8e7000ea60c1f79c27a952ee22271c227
Nicholas Turner <bindlez@gmail.com>
2024-05-17 00:03:55 -0400

    Time: 54 ms (71.43%), Space: 211.7 MB (42.86%) - LeetHub

--- 0832-flipping-an-image/0832-flipping-an-image.rb
+++ 0832-flipping-an-image/0832-flipping-an-image.rb
@@ -1,7 +1,7 @@
 # @param {Integer[][]} image
 # @return {Integer[][]}
 def flip_and_invert_image(image)
-    image.map(&:reverse).map do |ea|
-        ea.join.gsub('0', '2').gsub('1', '0').gsub('2', '1').chars.map(&:to_i)
-    end
+  image.map(&:reverse).map do |ea|
+  ea.map { |el| el == 0 ? 1 : 0 }
+  end
 end
