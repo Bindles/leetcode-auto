238ee575d33b23d00b51f036bf468fc90b735214
Nicholas Turner <bindlez@gmail.com>
2024-05-18 19:20:30 -0400

    Time: 8 ms (25%), Space: 2 MB (87.5%) - LeetHub

---
+++ 2716-minimize-string-length/2716-minimize-string-length.rs
@@ -0,0 +1,13 @@
+use std::collections::HashMap;
+
+impl Solution {
+    pub fn minimized_string_length(s: String) -> i32 {
+        let mut char_count = HashMap::new();
+        
+        for c in s.chars() {
+            *char_count.entry(c).or_insert(0) += 1;
+        }
+        
+        char_count.len() as i32
+    }
+}
