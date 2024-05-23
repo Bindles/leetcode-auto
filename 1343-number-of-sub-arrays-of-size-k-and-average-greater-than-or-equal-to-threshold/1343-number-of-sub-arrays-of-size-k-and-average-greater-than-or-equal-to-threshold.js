ad25f9ee2e4f5a7d757ef8c0fdfe6ee5aa489a52
Nicholas Turner <bindlez@gmail.com>
2024-05-22 20:21:54 -0400

    Time: 2286 ms (5.21%), Space: 55.9 MB (93.4%) - LeetHub

---
+++ 1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} arr
+ * @param {number} k
+ * @param {number} threshold
+ * @return {number}
+ */
+var numOfSubarrays = function(arr, k, threshold) {
+    let count = 0;
+    for (let i = 0; i <= arr.length - k; i++) {
+        let sum = 0;
+        for (let j = i; j < i + k; j++) {
+            sum += arr[j];
+        }
+        if (sum / k >= threshold) {
+            count++;
+        }
+    }
+    return count;
+}
