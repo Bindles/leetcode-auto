378e893327c018daaca0348b0478292babdeac85
Nicholas Turner <bindlez@gmail.com>
2024-05-23 23:42:42 -0400

    Time: 51 ms (70.22%), Space: 48.8 MB (50.22%) - LeetHub

--- 1450-number-of-students-doing-homework-at-a-given-time/1450-number-of-students-doing-homework-at-a-given-time.js
+++ 1450-number-of-students-doing-homework-at-a-given-time/1450-number-of-students-doing-homework-at-a-given-time.js
@@ -5,8 +5,13 @@
  * @return {number}
  */
 var busyStudent = function(startTime, endTime, queryTime) {
-    const times = startTime.map((start, index) => [start, endTime[index]]);
-    const count = times.filter(([a, b]) => queryTime >= a && queryTime <= b).length;
+    let count = 0;
+
+    for (let i = 0; i < startTime.length; i++) {
+        if (queryTime >= startTime[i] && queryTime <= endTime[i]) {
+            count++;
+        }
+    }
 
     return count;
 };
