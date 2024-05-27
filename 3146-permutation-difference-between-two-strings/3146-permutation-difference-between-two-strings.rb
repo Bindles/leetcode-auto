b3fc984304de20aa98ebb33ac49cdafe0c939c49
Nicholas Turner <bindlez@gmail.com>
2024-05-26 23:34:07 -0400

    Time: 59 ms (90.91%), Space: 211.4 MB (54.55%) - LeetHub

--- 3146-permutation-difference-between-two-strings/3146-permutation-difference-between-two-strings.rb
+++ 3146-permutation-difference-between-two-strings/3146-permutation-difference-between-two-strings.rb
@@ -2,9 +2,7 @@
 # @param {String} t
 # @return {Integer}
 def find_permutation_difference(s, t)
-  count=0
   sc = s.each_char.with_index.to_h
   tc = t.each_char.with_index.to_h
-  tc.each{|key,value| count += (tc[key] - sc[key]).abs }
-  count
+  tc.sum{|key,value| (tc[key] - sc[key]).abs }
 end
