9762607f1cdd0bf3f9fbae76a3d6aa5e9979aadb
Nicholas Turner <bindlez@gmail.com>
2024-05-28 04:02:48 -0400

    Time: 36 ms (47.54%), Space: 16.4 MB (60.37%) - LeetHub

--- 0140-word-break-ii/0140-word-break-ii.py
+++ 0140-word-break-ii/0140-word-break-ii.py
@@ -1,3 +1,12 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        return (f:=lambda i:[s[i:]]*(s[i:] in wordDict)+[s[i:j]+' '+t for j in range(i+1,len(s)) if s[i:j] in wordDict for t in f(j)])(0)
+        def f(i):
+            result = [s[i:]] if s[i:] in wordDict else []
+            for j in range(i+1, len(s)):
+                if s[i:j] in wordDict:
+                    for t in f(j):
+                        result.append(s[i:j]+' '+t)
+
+            return result
+
+        return f(0)
