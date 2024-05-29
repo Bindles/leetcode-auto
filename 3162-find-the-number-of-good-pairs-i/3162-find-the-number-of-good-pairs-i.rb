dfaddceed3779f7d66dd7c7bfbda7e98f7c32c15
Nicholas Turner <bindlez@gmail.com>
2024-05-29 14:59:20 -0400

    Time: 75 ms (21.43%), Space: 211.9 MB (100%) - LeetHub

--- 3162-find-the-number-of-good-pairs-i/3162-find-the-number-of-good-pairs-i.rb
+++ 3162-find-the-number-of-good-pairs-i/3162-find-the-number-of-good-pairs-i.rb
@@ -3,9 +3,5 @@
 # @param {Integer} k
 # @return {Integer}
 def number_of_pairs(nums1, nums2, k)
-    nums1.sum do |i|
-        nums2.count do |j|
-            i % (j * k) == 0
-        end
-    end
+    nums1.product(nums2).count {|a, b| a % (b * k) == 0 }  
 end
