ee51f4783f5aadcef8fb90e2d392c670b2d5a3a5
Nicholas Turner <bindlez@gmail.com>
2024-06-11 16:12:47 -0400

    Time: 110 ms (82.18%), Space: 216.1 MB (93.07%) - LeetHub

--- 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.rb
+++ 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.rb
@@ -2,19 +2,13 @@
 # @param {Integer} k
 # @return {Integer}
 def longest_ones(nums, k)
-  zero_count = 0
   left = 0
-  max = 0
-  
-  nums.each_with_index do |num, index|
-    zero_count+=1 if num==0
-
-    while zero_count > k
-      zero_count -= 1 if nums[left] == 0
-      left +=1
-    end
-
-    max = [index - left + 1, max].max
+  nums.each_index do |right|
+      k -= 1 - nums[right]
+      if k < 0
+          k += 1 - nums[left]
+          left+=1
+      end
   end
-  max
+  nums.size - left
 end
