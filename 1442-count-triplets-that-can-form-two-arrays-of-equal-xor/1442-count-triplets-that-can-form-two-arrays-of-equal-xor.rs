7adda8f30b04b6c13f6a731f1344546953020be6
Nicholas Turner <bindlez@gmail.com>
2024-06-14 10:42:03 -0400

    Time: 1 ms (68.67%), Space: 2.1 MB (90.66%) - LeetHub

---
+++ 1442-count-triplets-that-can-form-two-arrays-of-equal-xor/1442-count-triplets-that-can-form-two-arrays-of-equal-xor.rs
@@ -0,0 +1,16 @@
+impl Solution {
+    pub fn count_triplets(arr: Vec<i32>) -> i32 {
+        let mut result = 0 as i32;
+        let mut n = arr.len();
+        for i in 0..n {
+            let mut val = arr[i];
+            for k in i+1..n {
+                val ^= arr[k];
+                if val == 0 {
+                    result += (k - i) as i32;
+                }
+            }
+        }
+        result
+    }
+}
