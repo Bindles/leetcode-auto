7f9f764448843ae920be3bddcbbcc6971301329f
Nicholas Turner <bindlez@gmail.com>
2024-06-14 12:00:06 -0400

    Time: 74 ms (8.33%), Space: 211.6 MB (75%) - LeetHub

--- 1979-find-greatest-common-divisor-of-array/1979-find-greatest-common-divisor-of-array.rb
+++ 1979-find-greatest-common-divisor-of-array/1979-find-greatest-common-divisor-of-array.rb
@@ -1,13 +1,5 @@
 # @param {Integer[]} nums
 # @return {Integer}
 def find_gcd(nums)
-    low = nums.min
-    high = nums.max
-
-    while high % low != 0
-    p temp = low
-    p low = high % low
-    p high = temp
-    end
-    low
+    nums.min.gcd(nums.max)
 end
