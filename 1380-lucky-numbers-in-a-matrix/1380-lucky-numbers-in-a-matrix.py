31c8f5329f5112f854fb6260c77191d26661efee
Nicholas Turner <bindlez@gmail.com>
2024-06-24 06:50:04 -0400

    Time: 102 ms (90.42%), Space: 16.8 MB (84.42%) - LeetHub

--- 1380-lucky-numbers-in-a-matrix/1380-lucky-numbers-in-a-matrix.py
+++ 1380-lucky-numbers-in-a-matrix/1380-lucky-numbers-in-a-matrix.py
@@ -1,11 +1,3 @@
 class Solution:
     def luckyNumbers (self, matrix: List[List[int]]) -> List[int]:
-        # Transpose the matrix and find the maximum in each column
-        max_in_col = [max(col) for col in zip(*matrix)]
-
-        # Find the minimum in each row
-        min_in_row = [min(row) for row in matrix]
-
-        # Find the intersection of the two lists
-        result = list(set(min_in_row) & set(max_in_col))
-        return result
+      return list(set(min(row) for row in matrix) & set(max(col) for col in zip(*matrix)))
