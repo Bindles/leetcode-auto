b053b2aaa8ccc098aae13cd3b73e1ad1f8774822
Nicholas Turner <bindlez@gmail.com>
2024-06-26 18:33:19 -0400

    Time: 103 ms (48.24%), Space: 216.4 MB (83.42%) - LeetHub

--- 0055-jump-game/0055-jump-game.rb
+++ 0055-jump-game/0055-jump-game.rb
@@ -1,10 +1,11 @@
 # @param {Integer[]} nums
 # @return {Boolean}
 def can_jump(nums)
-  max_reach = i = 0
-  while i <= max_reach && max_reach < nums.size
-      max_reach = [max_reach, i + nums[i]].max
-      i += 1
+  max_reach = 0
+  (0...nums.size).each do |i|
+    return false if i > max_reach
+    max_reach = [max_reach, i + nums[i]].max
+    return true if max_reach >= nums.size - 1
   end
-  return max_reach >= nums.size - 1
+  false
 end
