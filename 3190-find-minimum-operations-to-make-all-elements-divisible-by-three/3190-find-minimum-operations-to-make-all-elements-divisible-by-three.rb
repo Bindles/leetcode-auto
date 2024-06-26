2e4af5ee13746f1c86fa6df9bf7bbfb0f28cdc2c
Nicholas Turner <bindlez@gmail.com>
2024-06-26 08:53:36 -0400

    Time: 63 ms (46.67%), Space: 211.9 MB (80%) - LeetHub

--- 3190-find-minimum-operations-to-make-all-elements-divisible-by-three/3190-find-minimum-operations-to-make-all-elements-divisible-by-three.rb
+++ 3190-find-minimum-operations-to-make-all-elements-divisible-by-three/3190-find-minimum-operations-to-make-all-elements-divisible-by-three.rb
@@ -1,9 +1,5 @@
 # @param {Integer[]} nums
 # @return {Integer}
 def minimum_operations(nums)
-    count=0
-    nums.each do |num|
-        count+=1 if num % 3 !=0
-    end
-    count 
+    nums.count {|num| num % 3 != 0 }
 end
