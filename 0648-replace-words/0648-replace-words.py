6eea3afc2d00b904d049303820ec55bf3469b8f6
Nicholas Turner <bindlez@gmail.com>
2024-06-26 01:16:10 -0400

    Time: 181 ms (57.5%), Space: 26 MB (36.02%) - LeetHub

--- 0648-replace-words/0648-replace-words.py
+++ 0648-replace-words/0648-replace-words.py
@@ -1,12 +1,17 @@
 class Solution:
     def replaceWords(self, dictionary: List[str], sentence: str) -> str:
+        result = ''
+        dictionary.sort(key=len)
         words = sentence.split(' ')
-        print(words)
-        for keyword in dictionary:
-            for i, word in enumerate(words):
-                if word.startswith(keyword):
-                    words[i] = keyword 
 
-        return ' '.join(words)
+        for word in words:
+            sub_str = word
+            for item in dictionary:
+                if sub_str.startswith(item):
+                    sub_str = item
+                    break
+            result += sub_str + ' '
+
+        return result.strip()
 
         
