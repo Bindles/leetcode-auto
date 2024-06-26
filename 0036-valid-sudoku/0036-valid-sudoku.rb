92dc3992db7f8499b9d1cd302f575549b649331a
Nicholas Turner <bindlez@gmail.com>
2024-06-26 15:50:14 -0400

    Time: 67 ms (82.68%), Space: 211.6 MB (60.89%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.rb
+++ 0036-valid-sudoku/0036-valid-sudoku.rb
@@ -1,47 +1,27 @@
 # @param {Character[][]} board
 # @return {Boolean}
 def is_valid_sudoku(board)
-  rows = board.length - 1
-  columns = board.length - 1
+    pos_array = Array.new(9)
+    
+    (0...9).each do |y|
+        (0...9).each do |x|
+            box_id = y/3*3+x/3
+            if board[y][x] != '.'
+                number = board[y][x].to_i          
 
-  # Function to validate a 3x3 sub-box
-  def valid_3x3(board, row, column)
-    visited = Set.new
-    (row...row + 3).each do |r|
-      (column...column + 3).each do |c|
-        next if board[r][c] == '.'
-        return false if visited.include?(board[r][c])
-        visited.add(board[r][c])
-      end
+                if pos_array[number]
+                    pos_array[number].each do |px,py|
+                        pbox_id = py/3*3+px/3
+                        if px == x || py == y || box_id == pbox_id
+                            return false
+                        end
+                    end
+                    pos_array[number] << [x,y]
+                else
+                    pos_array[number] = [[x,y]]
+                end
+            end
+        end
     end
     true
-  end
-
-  # Function to validate a row or a column
-  def valid_row_or_column(arr)
-    numeric = arr.select { |x| x != '.' }
-    numeric.length == numeric.uniq.length
-  end
-
-  # Validate all 3x3 sub-boxes
-  (0..rows).step(3).each do |row|
-    (0..columns).step(3).each do |column|
-      return false unless valid_3x3(board, row, column)
-    end
-  end
-
-  # Validate all rows
-  (0..rows).each do |row|
-    return false unless valid_row_or_column(board[row])
-  end
-
-  # Validate all columns
-  (0..columns).each do |column|
-    numeric = []
-    (0..rows).each do |row|
-      numeric << board[row][column]
-    end
-    return false unless valid_row_or_column(numeric)
-  end
-  true
 end
