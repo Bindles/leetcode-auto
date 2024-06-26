df59a85d3fc711e055ba7aa9f368c0194a9ccd67
Nicholas Turner <bindlez@gmail.com>
2024-06-26 18:42:08 -0400

    Time: 35 ms (92.04%), Space: 50.8 MB (81.64%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,13 +1,12 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        int maxr = 0;
-        int i = 0;
-        while (i<=maxr) {
-            maxr = max(maxr, i + nums[i]);
-            i++;
-            if (maxr >= nums.size()- 1) return true;
+        int max_reach = 0;
+        for (int i = 0; i < nums.size(); ++i) {
+            if (i > max_reach) return false;
+            max_reach = std::max(max_reach, i + nums[i]);
+            if (max_reach >= nums.size() - 1) return true;
         }
         return false;
-    }   
+    }
 };
