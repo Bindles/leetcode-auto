b7ac1275b7a9e3dcbaf054115329eceeeec95b9d
Nicholas Turner <bindlez@gmail.com>
2024-06-26 01:13:15 -0400

    Time: 611 ms (19.88%), Space: 225.8 MB (56.25%) - LeetHub

--- 0648-replace-words/0648-replace-words.rb
+++ 0648-replace-words/0648-replace-words.rb
@@ -2,5 +2,22 @@
 # @param {String} sentence
 # @return {String}
 def replace_words(dictionary, sentence)
-  dictionary.reduce(sentence.split) {|arr,keyword| arr.map {|word| word.start_with?(keyword) ? keyword : word } }.join ' '    
+  st = Set.new(dictionary)
+  words = sentence.split(' ')
+  result = []
+
+  words.each do |word|
+    str = ''
+    flag = false
+    word.each_char do |ch|
+      str += ch
+      if st.include?(str)
+        flag = true
+        break
+      end
+    end
+    result << (flag ? str : word)
+  end
+
+  result.join(' ')
 end
