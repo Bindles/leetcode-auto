4b56e05b5b37c870d39963ad41d7fb39a3f86727
Nicholas Turner <bindlez@gmail.com>
2024-06-27 16:13:23 -0400

    Time: 31 ms (99.06%), Space: 17.7 MB (65.99%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
@@ -1,8 +1,4 @@
 class Solution:
     def frequencySort(self, s: str) -> str:
-        scounter = Counter(s) #tally freq of chars in s
-        # Sort characters by frequency in descending order
-        sorted_scounter = sorted(scounter.items(), key=lambda chr: -chr[1])
-        # Generate sorted string by repeating characters according to freq
-        sorted_string = ''.join(char*freq for char,freq in sorted_scounter)
-        return sorted_string
+        # Sort chars by frequency in descending order and generate sorted string
+        return ''.join(char * freq for char, freq in sorted(Counter(s).items(), key=lambda x: -x[1]))
