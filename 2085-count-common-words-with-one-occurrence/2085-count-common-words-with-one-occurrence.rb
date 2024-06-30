35da0b8254855b29bc03f98e932a03aee13c5bc8
Nicholas Turner <bindlez@gmail.com>
2024-06-29 21:05:33 -0400

    Time: 86 ms (25%), Space: 240.3 MB (25%) - LeetHub

--- 2085-count-common-words-with-one-occurrence/2085-count-common-words-with-one-occurrence.rb
+++ 2085-count-common-words-with-one-occurrence/2085-count-common-words-with-one-occurrence.rb
@@ -2,8 +2,5 @@
 # @param {String[]} words2
 # @return {Integer}
 def count_words(words1, words2)
-    sentences_combined = words1 + words2
-    (words1 & words2).count do |w|
-        sentences_combined.count(w) == 2
-    end
+    (words1 & words2).count {|w|(words1 + words2).count(w) == 2}
 end
