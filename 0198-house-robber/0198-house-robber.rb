9521241740f8299f8cd17f2c36de94b567948767
Nicholas Turner <bindlez@gmail.com>
2024-07-01 18:29:20 -0400

    Time: 52 ms (83.33%), Space: 211.1 MB (60.26%) - LeetHub

--- 0198-house-robber/0198-house-robber.rb
+++ 0198-house-robber/0198-house-robber.rb
@@ -3,10 +3,7 @@
 def rob(nums)
     rob1 = rob2 = 0
     nums.each do |num|
-        # rob1, rob2 = rob2, [num + rob1, rob2].max # assign both at once
-        temp = [num + rob1, rob2].max
-        rob1 = rob2
-        rob2 = temp
+        rob1, rob2 = rob2, [num + rob1, rob2].max
     end
     rob2
 end
