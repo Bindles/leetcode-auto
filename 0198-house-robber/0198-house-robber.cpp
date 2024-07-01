2607b187945bf910be8e53b60d7fc82235e3d7c4
Nicholas Turner <bindlez@gmail.com>
2024-07-01 18:36:10 -0400

    Time: 0 ms (100%), Space: 9 MB (81.85%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,12 +1,15 @@
 class Solution {
+private:
+
 public:
     int rob(vector<int>& nums) {
-        int rob1 = 0;
-        int rob2 = 0;
-        for (int num : nums) {
-            int temp = max(num + rob1, rob2);
-            rob1 = rob2;
-            rob2 = temp;
+        int rob1=0;
+        int rob2=nums[0];
+        int temp=0;
+        for(int i=1;i<nums.size();i++){
+            temp=max(rob2,rob1+nums[i]);
+            rob1=rob2;
+            rob2=temp;
         }
         return rob2;
     }
