e0ba25ae3212b4635848a7f483149cd8d68d7ba7
Nicholas Turner <bindlez@gmail.com>
2024-07-06 23:28:27 -0400

    Time: 116 ms (69.37%), Space: 105.3 MB (61.6%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -3,16 +3,16 @@ public:
     vector<int> dailyTemperatures(vector<int>& temperatures) {
         int n = temperatures.size();
         vector<int> answer(n, 0);  // Initialize the result array with 0s
-        stack<int> s;  // Stack to store indices of the temperatures
+        stack<int> stack;  // Stack to store indices of the temperatures
 
         for (int i = 0; i < n; ++i) {
             // While stack is not empty and current temperature is greater than the temperature at the index stored at the top of the stack
-            while (!s.empty() && temperatures[i] > temperatures[s.top()]) {
-                int last = s.top();  // Get the index from the top of the stack
-                s.pop();  // Pop the index from the stack
+            while (!stack.empty() && temperatures[i] > temperatures[stack.top()]) {
+                int last = stack.top();  // Get the index from the top of the stack
+                stack.pop();  // Pop the index from the stack
                 answer[last] = i - last;  // Calculate the number of days and update the result array
             }
-            s.push(i);  // Push the current index onto the stack
+            stack.push(i);  // Push the current index onto the stack
         }
 
         return answer;
