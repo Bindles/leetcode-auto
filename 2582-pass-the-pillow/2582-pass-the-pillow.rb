df422d9332d899da4aa2f5d92d4a571679eec040
Nicholas Turner <bindlez@gmail.com>
2024-07-08 04:48:05 -0400

    Time: 56 ms (67.44%), Space: 211.3 MB (23.26%) - LeetHub

--- 2582-pass-the-pillow/2582-pass-the-pillow.rb
+++ 2582-pass-the-pillow/2582-pass-the-pillow.rb
@@ -5,10 +5,6 @@ def pass_the_pillow(n, time)
     return time+1 if time < n
     rounds_completed = time / (n-1) # #of compl rounds
     time_left = time % (n-1) #time rem after compl rounds
-    #0=>forward|1=>backward|2=>forward|3=>backward
-    if rounds_completed.even? # direction?(even=forward)
-        return time_left+1
-    else
-        return n - time_left
-    end
+    # direction?(even=forward)0=>forward|1=>backward|2=>f
+    rounds_completed.even? ? (return time_left+1) : (return n - time_left)
 end
