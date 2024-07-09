3adbd26d5b64e7b36723305ffa7eab432f0678dd
Nicholas Turner <bindlez@gmail.com>
2024-07-09 19:47:44 -0400

    Time: 53 ms (91.1%), Space: 211.6 MB (48.69%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.rb
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.rb
@@ -2,12 +2,8 @@
 # @param {String} str2
 # @return {String}
 def gcd_of_strings(str1, str2)
-    return "" unless str1 + str2 == str2 + str1
-    res=[]
-    count1 = str1.chars.tally
-    count2 = str2.chars.tally
-    count2.each  do |key,val| 
-        res << key if count1[key] &.> 1
-    end
-    res.join
+  return "" unless str1 + str2 == str2 + str1
+  
+  gcd_length = str1.length.gcd(str2.length)
+  str1[0, gcd_length]
 end
