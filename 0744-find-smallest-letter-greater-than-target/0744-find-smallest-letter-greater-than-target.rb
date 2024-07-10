2065828556bf6e03ce852cc9afb135657439937b
Nicholas Turner <bindlez@gmail.com>
2024-07-10 04:04:44 -0400

    Time: 63 ms (78.57%), Space: 212 MB (7.14%) - LeetHub

--- 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.rb
+++ 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.rb
@@ -2,6 +2,15 @@
 # @param {Character} target
 # @return {Character}
 def next_greatest_letter(letters, target)
-  letters.bsearch {|num| num > target } || letters[0]
+    low = 0
+    high = letters.size-1
+    while(low <= high)
+        mid = (low + high) / 2
+        if(letters[mid] > target)
+          high = mid - 1
+        else
+          low = mid + 1
+        end
+    end
+    return letters[low % letters.size]
 end
-
