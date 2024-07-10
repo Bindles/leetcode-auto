0501e74fabf751ed46495093b0b45cb83485bb66
Nicholas Turner <bindlez@gmail.com>
2024-07-09 22:43:57 -0400

    Time: 97 ms (7.22%), Space: 211.9 MB (97.42%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.rb
+++ 0605-can-place-flowers/0605-can-place-flowers.rb
@@ -1,10 +1,16 @@
 # @param {Integer[]} flowerbed
 # @param {Integer} n
 # @return {Boolean}
-
 def can_place_flowers(flowerbed, n)
   flowerbed.each_index do |i|
-    n-=1 if flowerbed[ [0, i-1].max..i+1 ].all?(0)   
+    if n < 1
+      break
+    elsif flowerbed[[0, i-1].max..i+1].all?(0) 
+      flowerbed[i] = 1
+      n-=1
+    end 
   end
   return n < 1
 end
+
+
