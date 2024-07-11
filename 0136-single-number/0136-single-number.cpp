473ffbfd3e1bcef24ac6c9df8351215ec7e0e764
Nicholas Turner <bindlez@gmail.com>
2024-07-10 21:03:21 -0400

    Time: 623 ms (5.15%), Space: 19.1 MB (98.56%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,11 +1,9 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        for (int num : nums) {
-            if (count(nums.begin(), nums.end(), num) < 2) {
-                return num;
-            }
-        }
-        return 0;
+        auto it = find_if(nums.begin(), nums.end(), [&nums](int num) {
+            return count(nums.begin(), nums.end(), num) != 2;
+        });
+        return (it != nums.end()) ? *it : -1; // Return -1 if no elem|should not reach.
     }
 };
