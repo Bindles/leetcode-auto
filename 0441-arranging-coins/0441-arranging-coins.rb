af2c8d85d77f0943bf893354037dde0e4b50b7ed
Nicholas Turner <bindlez@gmail.com>
2024-07-12 01:39:01 -0400

    Time: 52 ms (93.75%), Space: 211.5 MB (43.75%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.rb
+++ 0441-arranging-coins/0441-arranging-coins.rb
@@ -3,11 +3,10 @@
 def arrange_coins(n)
   left = 1
   right = n
+
   while left <= right do
     mid = (left + right) / 2
     area = 0.5 * mid**2 + mid / 2.0
-
-    puts "left: #{left}, right: #{right}, mid: #{mid}, area: #{area}"
 
     if area == n
       return mid
@@ -16,8 +15,8 @@ def arrange_coins(n)
     else
       right = mid - 1
     end
+    
   end
 
-  puts "Final left: #{left}, right: #{right}"
   left - 1
 end
