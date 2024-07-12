dc36aaebe5a8a83e6f9d004e95c487401bfe6aaa
Nicholas Turner <bindlez@gmail.com>
2024-07-12 01:36:35 -0400

    Time: 90 ms (56.25%), Space: 212.2 MB (6.25%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.rb
+++ 0441-arranging-coins/0441-arranging-coins.rb
@@ -1,14 +1,23 @@
 # @param {Integer} n
 # @return {Integer}
 def arrange_coins(n)
-  stair_cost=1
-  stairs_completed=0
+  left = 1
+  right = n
+  while left <= right do
+    mid = (left + right) / 2
+    area = 0.5 * mid**2 + mid / 2.0
 
-  while n >= stair_cost
-    n -= stair_cost
-    stairs_completed+=1
-    stair_cost+=1
+    puts "left: #{left}, right: #{right}, mid: #{mid}, area: #{area}"
+
+    if area == n
+      return mid
+    elsif area < n
+      left = mid + 1
+    else
+      right = mid - 1
+    end
   end
 
-  stairs_completed
+  puts "Final left: #{left}, right: #{right}"
+  left - 1
 end
