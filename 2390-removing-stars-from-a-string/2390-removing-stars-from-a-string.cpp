984ebb785551e0e06cec9f9d1f7c66240d31aadb
Nicholas Turner <bindlez@gmail.com>
2024-07-13 16:50:09 -0400

    Time: 23 ms (99.99%), Space: 25.7 MB (90.21%) - LeetHub

--- 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.cpp
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.cpp
@@ -2,17 +2,10 @@ int init=[] {ios_base::sync_with_stdio(false);cin.tie(0);return 0;}();
 class Solution {
 public:
     string removeStars(string s) {
-        vector<int> stack;
-        string str="";
-        
-        for (char chr : s) {
-            if (chr == '*') stack.pop_back();
-            else stack.push_back(chr);
+        int i = 0;
+        for(char chr : s) {
+            chr != '*' ? s[i++] = chr : --i;
         }
-        
-        for (int i=0;i<stack.size();++i) {
-            str+=stack[i];
-        }
-        return str;
+        return s.substr(0, i);
     }
 };
