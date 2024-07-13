4432619e6853a9775235d53f744c3ee0ec19237d
Nicholas Turner <bindlez@gmail.com>
2024-07-13 03:36:43 -0400

    Time: 2054 ms (6.35%), Space: 288.6 MB (11.11%) - LeetHub

--- 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.rb
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.rb
@@ -2,10 +2,5 @@
 # @param {Integer} k
 # @return {Integer}
 def max_vowels(s, k)
-  count=0
-  (0..s.size-k).each do |i|
-    vc = s[i...i+k].count('aeiou')
-    count = [count, vc].max
-  end
-  count
+  (0..s.size-k).map {|i| s[i...i+k].count('aeiouu') }.max
 end
