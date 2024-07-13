5a4ddf2dc8c098c87e54407a601020622b6942b4
Nicholas Turner <bindlez@gmail.com>
2024-07-13 16:46:26 -0400

    Time: 662 ms (33.87%), Space: 224.8 MB (38.71%) - LeetHub

--- 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.rb
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.rb
@@ -1,9 +1,9 @@
 # @param {String} s
 # @return {String}
 def remove_stars(s)
-  stack=[]
+  i = 0;
   s.each_char do |c|
-    c == '*' ? stack.pop : stack << c
+      c != '*' ? (s[i] = c;i+=1) : (i-=1);
   end
-  stack.join
+  s[0...i]
 end
