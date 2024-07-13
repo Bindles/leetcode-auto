a8fb7842bfd925f0b6d2cb46d9be57fe3f36e5e4
Nicholas Turner <bindlez@gmail.com>
2024-07-13 02:48:42 -0400

    Time: 139 ms (62.7%), Space: 60.4 MB (67.71%) - LeetHub

--- 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.js
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.js
@@ -4,22 +4,22 @@
  * @return {number}
  */
 var maxOperations = function(nums, k) {
-    nums.sort()
+    nums.sort((a, b) => a - b); // Sort nums array in ascending order
     let count = 0;
     let left = 0;
-    let right = nums.length-1
+    let right = nums.length - 1; 
 
-    while (left<right) {
-        sum = nums[left] + nums[right]
-        if (sum == k) {
+    while (left < right) {
+        let sum = nums[left] + nums[right];
+        if (sum === k) {
             count++;
             left++;
             right--;
         } else if (sum < k) {
-            left++
+            left++;
         } else {
             right--;
         }
     }
-    return count
+    return count;
 };
