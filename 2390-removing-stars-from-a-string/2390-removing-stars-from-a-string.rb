6d56eb856587f8d815c2b5b5ffea5e11f4af2f65
Nicholas Turner <bindlez@gmail.com>
2024-07-13 18:59:27 -0400

    Time: 409 ms (91.94%), Space: 219 MB (91.94%) - LeetHub

--- 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.rb
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.rb
@@ -2,18 +2,17 @@
 # @param {String} s
 # @return {String}
 def remove_stars(s)
-  res = []
-  number_of_stars = 0
-
-  s.reverse.each_char do |c|
-    if c == '*'
-      number_of_stars += 1
-    elsif number_of_stars > 0
-      number_of_stars -= 1
+  a, b = s.split("*", 2)  # Splitting 's' into two parts at the first '*'
+  return a if b.nil?      # If there is no second part (no '*'), return 'a' as is
+  a.chop!                 # Remove the last character of 'a' (to remove the trailing '*')
+  
+  # Iterate through 'b' and append characters to 'a', skipping any '*'
+  b.each_char do |char|
+    if char == "*"
+      a.chop!              # Remove the last character of 'a' if it's '*'
     else
-      res.push(c)
+      a << char            # Append 'char' to 'a' if it's not '*'
     end
   end
-
-  res.reverse.join
+  a                       # Return the modified 'a'
 end
