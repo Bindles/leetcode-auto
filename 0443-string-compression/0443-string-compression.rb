6205403a1934aeba63e88d08eb34704f377e4ba4
Nicholas Turner <bindlez@gmail.com>
2024-07-12 23:58:53 -0400

    Time: 79 ms (57.53%), Space: 212.3 MB (35.62%) - LeetHub

--- 0443-string-compression/0443-string-compression.rb
+++ 0443-string-compression/0443-string-compression.rb
@@ -1,13 +1,25 @@
-443. String Compression
-#* 443. String Compression
 # @param {Character[]} chars
 # @return {Integer}
 def compress(chars)
-    res=[]
-    x=chars.tally
-    x.each do |char,count|
-        res << char
-        res << count
+  i = res = 0
+  while i < chars.size
+    letter = chars[i]
+    count = 0
+
+    while i < chars.size and chars[i] == letter
+      count += 1
+      i += 1
     end
-    x
+    chars[res] = letter
+    res += 1
+        
+    if count > 1
+      #for digit in str(count):
+      count.to_s.each_char do |digit|
+        chars[res] = digit
+        res += 1
+      end
+    end
+  end
+  return res
 end
