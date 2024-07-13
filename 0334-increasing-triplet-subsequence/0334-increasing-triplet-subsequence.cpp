ed48e81e6a61abba88abb3d9399a7a97a8cfd970
Nicholas Turner <bindlez@gmail.com>
2024-07-12 23:04:50 -0400

    Time: 55 ms (99.36%), Space: 114.4 MB (10.52%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cpp
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cpp
@@ -2,16 +2,12 @@ int init=[] {ios_base::sync_with_stdio(false);cin.tie(0);return 0;}();
 class Solution {
 public:
     bool increasingTriplet(vector<int>& nums) {
-        for (int i=0;i<nums.size()-2;++i) {
-            for (int j=i+1;j<nums.size()-1;++j) {
-                for (int k=j+1;k<nums.size();++k) {
-                    if (nums[i] < nums[j] && nums[j] < nums[k]) {
-                        return true;
-                    }
-                }
-            }
+        int a = INT_MAX, b = INT_MAX;
+        for ( int num : nums) {
+          if (num <= a) a = num;
+          else if (num <= b) b = num;
+          else return true;
         }
-
         return false;       
     }
 };
