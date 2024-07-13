31fb1319985114faf151a153d551355e3e0e9676
Nicholas Turner <bindlez@gmail.com>
2024-07-13 07:02:56 -0400

    Time: 515 ms (25.25%), Space: 21.5 MB (43.53%) - LeetHub

--- 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.py
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.py
@@ -8,7 +8,6 @@ class Solution:
 
         for i in range(len(grid)):
             for j in range(len(grid)):
-                print(f"Comparing {grid[i]} with {c[j]}")
                 if grid[i] == c[j]:
                     count += 1
         return count
