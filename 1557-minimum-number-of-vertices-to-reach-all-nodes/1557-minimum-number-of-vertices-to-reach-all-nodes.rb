66b9805afd3db5a358b612f85e2d0ffc6c60a7e7
Nicholas Turner <bindlez@gmail.com>
2024-07-14 11:02:36 -0400

    Time: 169 ms (41.67%), Space: 222.9 MB (33.33%) - LeetHub

---
+++ 1557-minimum-number-of-vertices-to-reach-all-nodes/1557-minimum-number-of-vertices-to-reach-all-nodes.rb
@@ -0,0 +1,9 @@
+# @param {Integer} n
+# @param {Integer[][]} edges
+# @return {Integer[]}
+# @param {Integer} n
+# @param {Integer[][]} edges
+# @return {Integer[]}
+def find_smallest_set_of_vertices(n, edges)
+    (0...n).to_a - edges.map(&:last)
+end
