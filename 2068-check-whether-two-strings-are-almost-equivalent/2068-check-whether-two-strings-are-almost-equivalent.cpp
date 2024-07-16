4940f89c5de925dcdb249208ea5300e994209e52
Nicholas Turner <bindlez@gmail.com>
2024-07-16 11:40:13 -0400

    Time: 2 ms (57.07%), Space: 9.2 MB (11.14%) - LeetHub

---
+++ 2068-check-whether-two-strings-are-almost-equivalent/2068-check-whether-two-strings-are-almost-equivalent.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool checkAlmostEquivalent(string word1, string word2) {
+        unordered_map<char, int> charCount;
+        for (char key : word1) charCount[key]++; // Count chars in word1
+        // Subtract count for characters in word2
+        for (char key : word2) charCount[key]--; 
+        // Check if the absolute difference exceeds 3 for any character
+        for (auto& pair : charCount) {
+            if (abs(pair.second) > 3) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
