2e18966c02bd95d1aa86a61d42fd581327f0f40b
Nicholas Turner <bindlez@gmail.com>
2024-07-16 20:44:30 -0400

    Time: 69 ms (40%), Space: 211.5 MB (37.14%) - LeetHub

--- 0726-number-of-atoms/0726-number-of-atoms.rb
+++ 0726-number-of-atoms/0726-number-of-atoms.rb
@@ -1,12 +1,16 @@
 # @param {String} formula
 # @return {String}
 def count_of_atoms(formula)
+  # Scan for elements and their counts
   tokens = formula.scan(/(\W|[A-Z][a-z]*)(\d*)/)
   
+  # Reduce the tokens to count elements
   stack = tokens.reduce([Hash.new(0)]) do |stack, (element, count)|
     if element == '('
+      # If the element is '(', push a new hash onto the stack
       stack << Hash.new(0)
     elsif element == ')'
+      # If the element is ')', pop the last hash and update counts in the previous hash
       last_counts = stack.pop
       multiplier = count.to_i > 0 ? count.to_i : 1
 
@@ -15,6 +19,7 @@ def count_of_atoms(formula)
       end
       
     else
+      # For regular elements, update the counts
       count = count.to_i > 0 ? count.to_i : 1
       stack.last[element] += count
     end
@@ -22,6 +27,7 @@ def count_of_atoms(formula)
     stack
   end
 
+  # Get the final counts, sort, and format the output
   final_counts = stack.pop.sort
   final_counts.map { |element, count| count > 1 ? "#{element}#{count}" : element }.join
 end
