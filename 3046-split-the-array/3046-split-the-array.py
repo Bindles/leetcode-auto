20ac35fd1d1930ae2bd3939189ed606af95db519
Nicholas Turner <bindlez@gmail.com>
2024-07-20 22:09:14 -0400

    Time: 45 ms (70.12%), Space: 16.4 MB (94.29%) - LeetHub

--- 3046-split-the-array/3046-split-the-array.py
+++ 3046-split-the-array/3046-split-the-array.py
@@ -1,5 +1,5 @@
 class Solution:
     def isPossibleToSplit(self, nums: List[int]) -> bool:
-        return max(Counter(nums).values()) <= 2
+        return all(count <=2 for _,count in Counter(nums).items())
         
 
