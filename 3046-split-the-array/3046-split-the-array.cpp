3666f549e4f78083a134067fe96149ac283b6a4c
Nicholas Turner <bindlez@gmail.com>
2024-07-20 21:58:48 -0400

    Time: 9 ms (32.01%), Space: 25.6 MB (11.37%) - LeetHub

--- 3046-split-the-array/3046-split-the-array.cpp
+++ 3046-split-the-array/3046-split-the-array.cpp
@@ -3,10 +3,12 @@ public:
     bool isPossibleToSplit(vector<int>& nums) {
         unordered_map<int, int> nums_counts;
         for (int num : nums) nums_counts[num]++;
-
-        return all_of(nums_counts.begin(), nums_counts.end(), [](const auto& pair) {
-            return pair.second <= 2;
+        
+        auto max_count = max_element(nums_counts.begin(), nums_counts.end(), [](const auto& a, const auto& b) {
+            return a.second < b.second;
         });
+
+        return max_count != nums_counts.end() && max_count->second <= 2;
     }
 };
 
