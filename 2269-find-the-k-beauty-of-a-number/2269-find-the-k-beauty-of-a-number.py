90373adf6ee952297fd9ac981d729690fb0f4132
Nicholas Turner <bindlez@gmail.com>
2024-07-22 05:29:32 -0400

    Time: 43 ms (6.31%), Space: 16.5 MB (87.7%) - LeetHub

--- 2269-find-the-k-beauty-of-a-number/2269-find-the-k-beauty-of-a-number.py
+++ 2269-find-the-k-beauty-of-a-number/2269-find-the-k-beauty-of-a-number.py
@@ -1,11 +1,10 @@
 class Solution:
     def divisorSubstrings(self, num: int, k: int) -> int:  
         count=0     
-        nums = [(digit) for digit in str(num)]
+        nums = str(num)
         for i in range(len(nums)-(k-1)):
             window = int(''.join(nums[i:k+i]))
-            if window != 0:
-                if (num % window) == 0:
-                    count+=1
+            if window != 0 and (num % window) == 0:
+                count+=1
 
         return count
