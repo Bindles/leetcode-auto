c1685377392a546bae7bebd9acf0742d7d79d56a
Nicholas Turner <bindlez@gmail.com>
2024-07-22 05:04:52 -0400

    Time: 48 ms (94.44%), Space: 211.5 MB (11.11%) - LeetHub

--- 2269-find-the-k-beauty-of-a-number/2269-find-the-k-beauty-of-a-number.rb
+++ 2269-find-the-k-beauty-of-a-number/2269-find-the-k-beauty-of-a-number.rb
@@ -2,15 +2,11 @@
 # @param {Integer} k
 # @return {Integer}
 def divisor_substrings(num, k)
-    count=0
     nums=num.digits.reverse
-    (nums.size-(k-1)).times do |i|
-        window = nums[i, k]
-        p windowx = window.join.to_i
-        if windowx.nonzero?
-            count+=1 if (num % windowx) == 0
-            p "#{(num % windowx == 0)} | #{count}"
+    (0...nums.size-(k-1)).count do |i|
+        window = nums[i, k].join.to_i
+        if window.nonzero?
+            (num % window) == 0
         end
     end
-    count
 end
