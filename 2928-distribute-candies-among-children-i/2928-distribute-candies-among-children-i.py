350a9885d2281df8ae93b75468d5b96078a2b763
Nicholas Turner <bindlez@gmail.com>
2024-07-24 03:16:30 -0400

    Time: 45 ms (85.27%), Space: 16.5 MB (64.91%) - LeetHub

--- 2928-distribute-candies-among-children-i/2928-distribute-candies-among-children-i.py
+++ 2928-distribute-candies-among-children-i/2928-distribute-candies-among-children-i.py
@@ -1,11 +1,9 @@
 class Solution:
     def distributeCandies(self, n: int, limit: int) -> int:
-        res = 0
-        for i in range(limit + 1):
-            min_candies = max(0, n - i - limit)
-            max_candies = min(limit, n - i)
-            res += max(0, max_candies - min_candies + 1)
-        return res
+        return sum(
+            max(0, min(limit, n - i) - max(0, n - i - limit) + 1)
+            for i in range(limit + 1) 
+        )
 
 
 
