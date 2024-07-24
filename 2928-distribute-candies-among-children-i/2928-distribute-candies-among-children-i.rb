cd00400ba931349f89945a7ca4c52005e1966a1b
Nicholas Turner <bindlez@gmail.com>
2024-07-24 03:09:37 -0400

    Time: 75 ms (100%), Space: 211.4 MB (100%) - LeetHub

--- 2928-distribute-candies-among-children-i/2928-distribute-candies-among-children-i.rb
+++ 2928-distribute-candies-among-children-i/2928-distribute-candies-among-children-i.rb
@@ -5,7 +5,7 @@ def distribute_candies(n, limit)
   (0..limit).sum do |i|
     min_candies = [0, n - i - limit].max
     max_candies = [limit, n - i].min
-    (max_candies-min_candies < 0 ? 0 : ((max_candies-min_candies) + 1))
+    (max_candies >= min_candies) ? (max_candies - min_candies + 1) : 0;
   end
 end
 
