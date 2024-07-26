baf4b753d17a88113957a06112e4d2aa84dc815f
Nicholas Turner <bindlez@gmail.com>
2024-07-26 11:46:46 -0400

    Time: 11 ms (20.88%), Space: 10.7 MB (81.41%) - LeetHub

--- 2696-minimum-string-length-after-removing-substrings/2696-minimum-string-length-after-removing-substrings.cpp
+++ 2696-minimum-string-length-after-removing-substrings/2696-minimum-string-length-after-removing-substrings.cpp
@@ -1,14 +1,15 @@
-int init=[] {ios_base::sync_with_stdio(false);cin.tie(0);return 0;}();
 class Solution {
 public:
     int minLength(string s) {
         vector<char> stack;
         for (char chr : s) {
-            if (!stack.empty() && (chr == 'B' && stack.back() == 'A' || chr == 'D' && stack.back() == 'C')) {
-                stack.pop_back();
-            } else {
-                stack.push_back(chr);
+            if (!stack.empty()) {
+                if (chr == 'B' && stack.back() == 'A' || chr == 'D' && stack.back() == 'C') {
+                    stack.pop_back();
+                    continue;
+                }
             }
+            stack.push_back(chr); 
         }
         return stack.size();
     }
