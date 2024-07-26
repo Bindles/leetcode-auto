063e8d05212783f36fa89a533c4637e10504de77
Nicholas Turner <bindlez@gmail.com>
2024-07-26 10:00:33 -0400

    Time: 61 ms (100%), Space: 211.5 MB (100%) - LeetHub

--- 2129-capitalize-the-title/2129-capitalize-the-title.rb
+++ 2129-capitalize-the-title/2129-capitalize-the-title.rb
@@ -3,12 +3,7 @@
 def capitalize_title(title)
     words = title.split(' ')
     words.each_index do |i|
-        if words[i].size <=2 
-            words[i].downcase!
-        else
-            words[i].downcase!
-            words[i][0] = words[i][0].upcase
-        end
+        words[i].size <=2 ? words[i].downcase! : words[i].capitalize!
     end
     words.join(' ')
 end
