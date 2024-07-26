de20cc27a5b41e2c06253d14d6e0dfc9b12352cf
Nicholas Turner <bindlez@gmail.com>
2024-07-26 14:49:30 -0400

    Time: 0 ms (100%), Space: 7.3 MB (60.47%) - LeetHub

--- 1884-egg-drop-with-2-eggs-and-n-floors/1884-egg-drop-with-2-eggs-and-n-floors.cpp
+++ 1884-egg-drop-with-2-eggs-and-n-floors/1884-egg-drop-with-2-eggs-and-n-floors.cpp
@@ -1,6 +1,11 @@
 class Solution {
 public:
     int twoEggDrop(int n) {
-       return ceil((sqrt(1+8*n) -1)/2);
+        int res=0;
+        while(n>0) {
+            res++;
+            n -= res;
+        }
+        return res;
     }
 };
