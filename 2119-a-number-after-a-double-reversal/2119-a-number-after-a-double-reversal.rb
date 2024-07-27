90d9fbc75ff7e8a6613692fbf53615e41f9c2c57
Nicholas Turner <bindlez@gmail.com>
2024-07-27 09:36:26 -0400

    Time: 72 ms (28.57%), Space: 211.2 MB (100%) - LeetHub

--- 2119-a-number-after-a-double-reversal/2119-a-number-after-a-double-reversal.rb
+++ 2119-a-number-after-a-double-reversal/2119-a-number-after-a-double-reversal.rb
@@ -1,5 +1,6 @@
 # @param {Integer} num
 # @return {Boolean}
 def is_same_after_reversals(num)
-    num.to_s.reverse.to_i.to_s.reverse.to_i == num
+    numrev = num.to_s.reverse.to_i
+    numrev.to_s.reverse.to_i == num
 end
