4aa81a316f5979ba1202bf2cc9b7468cbf65004d
Nicholas Turner <bindlez@gmail.com>
2024-07-28 13:22:33 -0400

    Time: 21 ms (96.79%), Space: 14.7 MB (18.91%) - LeetHub

--- 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.cpp
+++ 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.cpp
@@ -2,21 +2,19 @@ int init=[] {ios_base::sync_with_stdio(false);cin.tie(0);return 0;}();
 class Solution {
 public:
     int minDeletionSize(vector<string>& strs) {
-        int rows = strs.size(), cols = strs[0].size();
-        vector<string> transposed(cols, string(rows, ' '));
-        int count=0;
-        
-        for (int i=0;i<rows;++i) {
-            for (int j=0;j<cols;++j) {
-                transposed[j][i] = strs[i][j];
+        int rows = strs.size();
+        int cols = strs[0].size();
+        int count = 0;
+
+        for (int j = 0; j < cols; ++j) {
+            for (int i = 1; i < rows; ++i) {
+                if (strs[i][j] < strs[i - 1][j]) {
+                    ++count;
+                    break;
+                }
             }
         }
 
-        for (const auto& col : transposed) { //string
-            if (!is_sorted(col.begin(), col.end() ) ) {
-                count++;
-            }
-        }
         return count;
     }
 };
