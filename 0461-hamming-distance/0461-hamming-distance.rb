91fc717df858bdc0fdffd1ad48ddbbad85740e89
Nicholas Turner <bindlez@gmail.com>
2024-07-28 11:23:49 -0400

    Time: 54 ms (69.23%), Space: 211.4 MB (19.23%) - LeetHub

--- 0461-hamming-distance/0461-hamming-distance.rb
+++ 0461-hamming-distance/0461-hamming-distance.rb
@@ -2,14 +2,11 @@
 # @param {Integer} y
 # @return {Integer}
 def hamming_distance(x, y)
-    count=0
     max_bit_length=[x.bit_length, y.bit_length].max
-    
     xb = x.to_s(2).rjust(max_bit_length, '0')
     xy = y.to_s(2).rjust(max_bit_length, '0')
-    xb.chars.each_index do |i|
-        puts  "#{xb[i]} | #{xy[i]}"
-        count+=1 if xb[i] != xy[i]
+    
+    xb.chars.each_index.count do |i|
+        xb[i] != xy[i]
     end
-    count 
 end
