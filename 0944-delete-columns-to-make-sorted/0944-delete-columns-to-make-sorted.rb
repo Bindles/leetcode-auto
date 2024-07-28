699098438ca9076092310da2dc071a73884269e9
Nicholas Turner <bindlez@gmail.com>
2024-07-28 13:23:17 -0400

    Time: 251 ms (100%), Space: 228.3 MB (35.71%) - LeetHub

--- 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.rb
+++ 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.rb
@@ -1,5 +1,10 @@
 # @param {String[]} strs
 # @return {Integer}
 def min_deletion_size(strs)
-    str_columns=strs.map{|str|str.chars}.transpose.count{|col|col.sort!=col}  
+    # Convert (map) the array of strings into an array of character arrays
+    # Get columns by taking indice 0-str.length of each row (transpose)
+    str_columns = strs.map { |str| str.chars }.transpose
+    str_columns.count do |col|
+        col.sort != col
+    end   
 end 
