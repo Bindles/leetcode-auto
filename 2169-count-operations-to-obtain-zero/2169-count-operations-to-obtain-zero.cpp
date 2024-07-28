5fcabd69b716a153956425100a455d6e63ada82c
Nicholas Turner <bindlez@gmail.com>
2024-07-28 15:12:01 -0400

    Time: 0 ms (100%), Space: 7.7 MB (14.39%) - LeetHub

---
+++ 2169-count-operations-to-obtain-zero/2169-count-operations-to-obtain-zero.cpp
@@ -0,0 +1,12 @@
+int init=[] {ios_base::sync_with_stdio(false);cin.tie(0);return 0;}();
+class Solution {
+public:
+    int countOperations(int num1, int num2) {
+        int count=0;
+        while (num1!=0 && num2!=0) {
+            num1 >= num2 ? num1 -= num2 : num2 -= num1;
+            count+=1;
+        }
+        return count;
+    }
+};
