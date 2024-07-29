75816e56fb89970257f0afb0d628076ac118d93b
Nicholas Turner <bindlez@gmail.com>
2024-07-29 08:59:53 -0400

    Time: 7 ms (48.31%), Space: 26.6 MB (7.78%) - LeetHub

--- 3206-alternating-groups-i/3206-alternating-groups-i.cpp
+++ 3206-alternating-groups-i/3206-alternating-groups-i.cpp
@@ -2,13 +2,13 @@ int init=[]{ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);return 0;}()
 class Solution {
 public:
     int numberOfAlternatingGroups(vector<int>& colors) {
-        int count =0, n= colors.size();
-        for(int i=0;i<n;i++) {
-             if(colors[i] != colors[(i+1)%n] && colors[(i+1)%n] != colors[(i+2)%n]) count++;
+        int count = 0;
+        int n = colors.size();
+        for (int i = 0; i < n; i++) {
+            if (colors[(i-1 + n) % n] != colors[i] && colors[i] != colors[(i+1) % n]) {
+                count++;
+            }
         }
         return count;
     }
 };
-
-
-
