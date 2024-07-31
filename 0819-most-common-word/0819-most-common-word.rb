146c6b5008b02dac9f18d1912adb5f20225760a1
Nicholas Turner <bindlez@gmail.com>
2024-07-31 15:18:50 -0400

    Time: 87 ms (8.7%), Space: 211.9 MB (47.83%) - LeetHub

--- 0819-most-common-word/0819-most-common-word.rb
+++ 0819-most-common-word/0819-most-common-word.rb
@@ -2,8 +2,11 @@
 # @param {String[]} banned
 # @return {String}
 def most_common_word(paragraph, banned)
-    para_clean = paragraph.downcase.gsub(',',' ').gsub('.',' ').gsub('!',' ').split(' ').tally.sort_by{|k,v| -v}
-    para_clean.each do |k,v|
-        return k if !banned.include?(k)
+    bset = Set.new(banned)
+    words = paragraph.split(/\W+/).map(&:downcase).reject{|x|bset.member?(x)}
+    wordtally = words.tally
+
+    words.max_by do |word| 
+        wordtally[word] 
     end
 end
