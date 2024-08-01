77b45ccf5e18aa63d6816e6a26928ec6154f80af
Nicholas Turner <bindlez@gmail.com>
2024-08-01 14:29:17 -0400

    Time: 504 ms (9.09%), Space: 214.8 MB (9.09%) - LeetHub

--- 0495-teemo-attacking/0495-teemo-attacking.rb
+++ 0495-teemo-attacking/0495-teemo-attacking.rb
@@ -6,7 +6,7 @@ def find_poisoned_duration(time_series, duration)
     time_series.each_index do |i|
         break if i == time_series.size - 1 # Stop before the last element
         p "#{duration} | #{time_series[i+1]} #{time_series[i]} => #{time_series[i+1] - time_series[i]}"
-        time_poisoned = [duration, (time_series[i+1]-time_series[i])].min
+        time_poisoned += [duration, (time_series[i+1]-time_series[i])].min
     end
     time_poisoned + duration
 end
