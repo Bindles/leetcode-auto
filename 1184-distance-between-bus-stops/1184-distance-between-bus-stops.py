50d1b14ba88a565287efa407f1f1627c6abaf469
Nicholas Turner <bindlez@gmail.com>
2024-08-01 13:54:19 -0400

    Time: 44 ms (78.22%), Space: 17.5 MB (69.29%) - LeetHub

--- 1184-distance-between-bus-stops/1184-distance-between-bus-stops.py
+++ 1184-distance-between-bus-stops/1184-distance-between-bus-stops.py
@@ -1,6 +1,7 @@
 class Solution:
     def distanceBetweenBusStops(self, distance, start, destination):
-        start, destination = min(start, destination), max(start, destination)
+        #start, destination = min(start, destination), max(start, destination)
+        if start > destination: start, destination = destination, start #maybe faster then^
         clockwise = sum(distance[start:destination])
         total = sum(distance)
         counterclockwise = total - clockwise
