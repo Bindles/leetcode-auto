96e7685ac99101abeaf03f0ec9a5ce8716c54b49
Nicholas Turner <bindlez@gmail.com>
2024-08-01 13:47:27 -0400

    Time: 69 ms (25%), Space: 211.5 MB (75%) - LeetHub

--- 1184-distance-between-bus-stops/1184-distance-between-bus-stops.rb
+++ 1184-distance-between-bus-stops/1184-distance-between-bus-stops.rb
@@ -3,6 +3,7 @@
 # @param {Integer} destination
 # @return {Integer}
 def distance_between_bus_stops(distance, start, destination)
+  start, destination = [start, destination].minmax
   clockwise = distance[start...destination].sum
   counterclockwise = distance.sum - clockwise
   [clockwise, counterclockwise].min
