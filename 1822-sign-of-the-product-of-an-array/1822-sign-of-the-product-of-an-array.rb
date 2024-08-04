771e045f3a72ffd909bc4284d73fe976db80c5e6
Nicholas Turner <bindlez@gmail.com>
2024-08-04 12:15:48 -0400

    Time: 73 ms (7.69%), Space: 211.7 MB (100%) - LeetHub

--- 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.rb
+++ 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.rb
@@ -1,13 +1,15 @@
 # @param {Integer[]} nums
 # @return {Integer}
 def array_sign(nums)
-    product = nums.reduce(:*)
-    case
-    when product > 0
-        return 1
-    when product == 0
-        return 0
-    when product < 0
-        return -1
+    negative_count=0
+
+    nums.each do |num|
+        if num == 0
+            return 0
+        elsif num < 0 
+            negative_count+=1
+        end
     end
+
+    (negative_count.even?) ? 1 : -1
 end
