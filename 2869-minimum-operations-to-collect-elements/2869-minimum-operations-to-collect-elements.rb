abcec6e12c71a208a6221b71cb97a71cf98a71e3
Nicholas Turner <bindlez@gmail.com>
2024-08-08 12:57:26 -0400

    Time: 66 ms (80%), Space: 211.9 MB (100%) - LeetHub

--- 2869-minimum-operations-to-collect-elements/2869-minimum-operations-to-collect-elements.rb
+++ 2869-minimum-operations-to-collect-elements/2869-minimum-operations-to-collect-elements.rb
@@ -1,13 +1,17 @@
 # @param {Integer[]} nums
 # @param {Integer} k
 # @return {Integer}
+# @param {Integer[]} nums
+# @param {Integer} k
+# @return {Integer}
 def min_operations(nums, k)
-    count = 0    
-    res = []
-    range = (1..k)
-    until range.all? {|x| res.include?(x)}
-        res << nums.pop
-        count+=1
+    count = 0
+    rng = Set.new(1..k)
+
+    until rng.empty?
+        num = nums.pop
+        rng.delete(num)
+        count += 1
     end
     count
 end
