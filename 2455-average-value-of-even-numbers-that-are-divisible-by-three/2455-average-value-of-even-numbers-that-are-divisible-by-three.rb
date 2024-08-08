3a9ab475ee6b45683c830a342f68246b56042f4a
Nicholas Turner <bindlez@gmail.com>
2024-08-08 11:35:15 -0400

    Time: 80 ms (50%), Space: 212.2 MB (50%) - LeetHub

--- 2455-average-value-of-even-numbers-that-are-divisible-by-three/2455-average-value-of-even-numbers-that-are-divisible-by-three.rb
+++ 2455-average-value-of-even-numbers-that-are-divisible-by-three/2455-average-value-of-even-numbers-that-are-divisible-by-three.rb
@@ -1,6 +1,13 @@
 # @param {Integer[]} nums
 # @return {Integer}
-def average_value(nums) # num.even? && num % 3 == 0 simplified below
-    selected_nums = nums.select{|num| num % 6 == 0}
-    selected_nums.empty? ? 0 : selected_nums.sum / selected_nums.size
+def average_value(nums) 
+    how_many_nums = total = 0
+    nums.each do |num|
+        if num % 6 == 0 # num %6 == 0 will be even and divisible by 3
+            total+=num
+            how_many_nums+=1
+        end
+    end
+    how_many_nums == 0 ? 0 : total / how_many_nums 
 end
+
