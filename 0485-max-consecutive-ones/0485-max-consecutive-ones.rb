a31c56176df9a50088794d021a789c7703d19d31
Nicholas Turner <bindlez@gmail.com>
2024-08-08 13:57:43 -0400

    Time: 107 ms (9.09%), Space: 216.9 MB (36.36%) - LeetHub

--- 0485-max-consecutive-ones/0485-max-consecutive-ones.rb
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.rb
@@ -1,6 +1,9 @@
 # @param {Integer[]} nums
 # @return {Integer}
 def find_max_consecutive_ones(nums)
-  max_ones=nums.chunk_while{|a,b|a==1 && b==1}.select { |chunk| chunk.first == 1 }.max_by(&:size)
-  max_ones.nil? ? 0 : max_ones.size
+  nums.chunk_while { |a, b| a == 1 && b == 1 } #Group consec 1s
+      .select { |chunk| chunk.first == 1 }     # Select chunks if 1
+      .map(&:size)                             # Map sizes of chunks
+      .max                                     # Find maximum size
+      .to_i                                    # Convert2int|0 if nil
 end
