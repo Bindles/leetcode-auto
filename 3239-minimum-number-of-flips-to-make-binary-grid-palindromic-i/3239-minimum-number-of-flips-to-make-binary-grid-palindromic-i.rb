6740b7a7c57b7987273adcf97547077279c30a6a
Nicholas Turner <bindlez@gmail.com>
2024-08-09 13:55:59 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 3239-minimum-number-of-flips-to-make-binary-grid-palindromic-i/3239-minimum-number-of-flips-to-make-binary-grid-palindromic-i.rb
@@ -0,0 +1,18 @@
+# @param {Integer[][]} grid
+# @return {Integer}
+def min_flips(grid)
+  n = grid.size / 2 
+  row_ans, col_ans = 0, 0
+  
+  row_ans = grid.sum { |row| (0...n).sum { |i| row[i] ^ row[-i-1] } }
+
+  grid = grid.transpose
+
+  grid.each do |row|
+    (0...n).each do |i|
+      col_ans += row[i] ^ row[-i-1]
+    end
+  end
+
+  [row_ans, col_ans].min
+end
