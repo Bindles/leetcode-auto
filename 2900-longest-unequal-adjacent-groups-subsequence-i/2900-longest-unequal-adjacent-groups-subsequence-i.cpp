adb5c305bd19ae486db78e1a077e5a0fdf903c6f
Nicholas Turner <bindlez@gmail.com>
2024-08-14 14:59:30 -0400

    Time: 11 ms (43.25%), Space: 29.1 MB (17.54%) - LeetHub

--- 2900-longest-unequal-adjacent-groups-subsequence-i/2900-longest-unequal-adjacent-groups-subsequence-i.cpp
+++ 2900-longest-unequal-adjacent-groups-subsequence-i/2900-longest-unequal-adjacent-groups-subsequence-i.cpp
@@ -1,3 +1,4 @@
+int init=[]{ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);return 0;}();
 class Solution {
 public:
     vector<string> getLongestSubsequence(vector<string>& words, vector<int>& groups) {
