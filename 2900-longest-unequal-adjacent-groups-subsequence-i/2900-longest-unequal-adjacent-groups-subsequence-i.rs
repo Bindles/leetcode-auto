fcf987c2e308bc37939da8cad8df14de7bc11bf4
Nicholas Turner <bindlez@gmail.com>
2024-08-14 15:03:04 -0400

    Time: 3 ms (68.75%), Space: 2.2 MB (68.75%) - LeetHub

--- 2900-longest-unequal-adjacent-groups-subsequence-i/2900-longest-unequal-adjacent-groups-subsequence-i.rs
+++ 2900-longest-unequal-adjacent-groups-subsequence-i/2900-longest-unequal-adjacent-groups-subsequence-i.rs
@@ -1,17 +1,13 @@
 impl Solution {
     pub fn get_longest_subsequence(words: Vec<String>, groups: Vec<i32>) -> Vec<String> {
         let mut subsequence = Vec::new();
-        let mut last_group = groups[0];
-        
         subsequence.push(words[0].clone());
-        
+
         for i in 1..words.len() {
-            if groups[i] != last_group {
+            if groups[i] != groups[i-1] {
                 subsequence.push(words[i].clone());
-                last_group = groups[i];
             }
         }
-        
         subsequence
     }
 }
