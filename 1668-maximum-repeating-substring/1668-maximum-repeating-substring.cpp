89fa319ec09234a8827deadfad8310dcac341ae4
Nicholas Turner <bindlez@gmail.com>
2024-08-15 15:35:53 -0400

    Time: 0 ms (100%), Space: 8.1 MB (50.29%) - LeetHub

---
+++ 1668-maximum-repeating-substring/1668-maximum-repeating-substring.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxRepeating(string sequence, string word) {
+      int k=0;
+      string repeated_word = word;
+
+      while(sequence.find(repeated_word) != string::npos)  {
+        k++;
+        repeated_word += word;
+      }
+      return k;
+    }
+};
