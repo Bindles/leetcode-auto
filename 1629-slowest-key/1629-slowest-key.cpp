730ad292e7c7be2b47402aba050d133afe1a873c
Nicholas Turner <bindlez@gmail.com>
2024-08-16 13:03:41 -0400

    Time: 6 ms (57.08%), Space: 13.5 MB (33.48%) - LeetHub

--- 1629-slowest-key/1629-slowest-key.cpp
+++ 1629-slowest-key/1629-slowest-key.cpp
@@ -2,14 +2,15 @@ int init=[]{ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);return 0;}()
 class Solution {
 public:
     char slowestKey(vector<int>& releaseTimes, string keysPressed) {
-        vector<pair<char,int>> pressTimes;
-        for (int i=0;i<releaseTimes.size();++i) {
+        int n = releaseTimes.size();
+        pair<char,int> pressTimes[n];
+        for (int i=0;i<n;++i) {
             int duration = (i == 0) ? releaseTimes[i] : releaseTimes[i] - releaseTimes[i-1];
-            pressTimes.emplace_back(keysPressed[i], duration);
+            pressTimes[i] = {keysPressed[i], duration};
         }
         
         // Sort first by descending time, then by descending key (lexicographical)
-        sort(pressTimes.begin(), pressTimes.end(), [](const auto& a, const auto& b) {
+        sort(pressTimes, pressTimes + n, [](const auto& a, const auto& b) {
             return (a.second > b.second) || (a.second == b.second && a.first > b.first);
         });
 
