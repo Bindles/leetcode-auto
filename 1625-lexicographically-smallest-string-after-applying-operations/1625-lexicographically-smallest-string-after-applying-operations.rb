e24f6745c84db6ecde1f891331c5832b94608ffb
Nicholas Turner <bindlez@gmail.com>
2024-08-16 07:03:28 -0400

    Time: 2418 ms (100%), Space: 219.8 MB (100%) - LeetHub

--- 1625-lexicographically-smallest-string-after-applying-operations/1625-lexicographically-smallest-string-after-applying-operations.rb
+++ 1625-lexicographically-smallest-string-after-applying-operations/1625-lexicographically-smallest-string-after-applying-operations.rb
@@ -17,10 +17,14 @@ def find_lex_smallest_string(s, a, b)
     modified = current.chars.map.with_index do |chr, i|
       i.odd? ? (chr.to_i + a) % 10 : chr.to_i
     end.join
+
+    # Only consider adding the modified string if it's new
     queue.push(modified) unless seen[modified]
 
     # Apply operation 2: Rotate string by `b` positions
-    rotated = current.chars.rotate(-b).join
+    rotated = current[-b..-1] + current[0...-b]
+
+    # Only consider adding the rotated string if it's new
     queue.push(rotated) unless seen[rotated]
   end
 
