f3cc45d314026cb1a9d7d7d858f39b60c739ccd3
Nicholas Turner <bindlez@gmail.com>
2024-08-17 16:03:53 -0400

    Time: 5 ms (36.3%), Space: 8.4 MB (36.3%) - LeetHub

---
+++ 1414-find-the-minimum-number-of-fibonacci-numbers-whose-sum-is-k/1414-find-the-minimum-number-of-fibonacci-numbers-whose-sum-is-k.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int findMinFibonacciNumbers(int k) {
+        // Generate Fibonacci numbers up to k
+        vector<int> fibs = {1, 1};
+        while (fibs.back() <= k) {
+            fibs.emplace_back(fibs.back() + fibs[fibs.size() - 2]);
+        }
+        
+        int count = 0;
+        int i = fibs.size() - 1;
+        
+        // Use the largest possible Fibonacci number and subtract it from k
+        while (k > 0) {
+            if (fibs[i] <= k) {
+                k -= fibs[i];
+                count++;
+            }
+            i--;
+        }
+        
+        return count;
+    }
+};
