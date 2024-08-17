0305e5857b20981cff12205287335fe5880d399d
Nicholas Turner <bindlez@gmail.com>
2024-08-17 16:01:55 -0400

    Time: 72 ms (12.5%), Space: 211.7 MB (87.5%) - LeetHub

--- 1414-find-the-minimum-number-of-fibonacci-numbers-whose-sum-is-k/1414-find-the-minimum-number-of-fibonacci-numbers-whose-sum-is-k.rb
+++ 1414-find-the-minimum-number-of-fibonacci-numbers-whose-sum-is-k/1414-find-the-minimum-number-of-fibonacci-numbers-whose-sum-is-k.rb
@@ -1,26 +1,24 @@
 # @param {Integer} k
 # @return {Integer}
 def find_min_fibonacci_numbers(k)
-    fib_array=[]
-    a,b = 0,1
-    fib_array << a
-    fib_array << b
-
-    (2..k).each do 
-        a,b = b, a+b
-        fib_array << b
+  # Generate Fibonacci numbers up to k
+  fibs = [1, 1]
+  while fibs[-1] <= k
+    fibs << fibs[-1] + fibs[-2]
+  end
+  
+  count = 0
+  i = fibs.length - 1
+  
+  # Use the largest possible Fibonacci number and subtract it from k
+  while k > 0
+    if fibs[i] <= k
+      k -= fibs[i]
+      count += 1
     end
-    
-    count = 0
-    i = fib_array.size-1
-
-    while k > 0
-        if fib_array[i] <= k
-            k -= fib_array[i]
-            count += 1
-        end
-        i -= 1
-    end
-    count
+    i -= 1
+  end
+  
+  count
 end
 
