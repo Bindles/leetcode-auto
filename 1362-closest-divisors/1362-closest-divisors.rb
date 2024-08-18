96daada0e4dc48414f2b6ac458c93d0ea24847c2
Nicholas Turner <bindlez@gmail.com>
2024-08-18 12:48:44 -0400

    Time: 126 ms (100%), Space: 211.1 MB (100%) - LeetHub

--- 1362-closest-divisors/1362-closest-divisors.rb
+++ 1362-closest-divisors/1362-closest-divisors.rb
@@ -1,24 +1,13 @@
 # @param {Integer} num
 # @return {Integer[]}
 def closest_divisors(num)
-  # Helper method to find the divisors closest to each other for a given target.
-  def find_divisors(target)
-    # Start from the square root of the target and move downwards.
-    (Math.sqrt(target).to_i).downto(1) do |i|
-      if target % i == 0
-        return [i, target / i]
-      end
+  (Math.sqrt(num + 2).to_i).downto(2) do |i|
+    if (num + 1) % i == 0
+      return [i, (num + 1) / i]
+    elsif (num + 2) % i == 0
+      return [i, (num + 2) / i]
     end
   end
   
-  # Check both num + 1 and num + 2
-  div1 = find_divisors(num + 1)
-  div2 = find_divisors(num + 2)
-  
-  # Return the pair with the smallest absolute difference
-  if (div1[0] - div1[1]).abs < (div2[0] - div2[1]).abs
-    div1
-  else
-    div2
-  end
+  [1, num + 1]
 end
