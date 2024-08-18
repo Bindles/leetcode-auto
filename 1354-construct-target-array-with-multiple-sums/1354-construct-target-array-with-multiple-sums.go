8f77b19989a97d34010785172ee5f8fc68f09303
Nicholas Turner <bindlez@gmail.com>
2024-08-18 15:25:29 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1354-construct-target-array-with-multiple-sums/1354-construct-target-array-with-multiple-sums.go
+++ 1354-construct-target-array-with-multiple-sums/1354-construct-target-array-with-multiple-sums.go
@@ -1,22 +1,45 @@
+package main
+
 func isPossible(target []int) bool {
-	sort.Sort(sort.Reverse(sort.IntSlice(target)))
 	sum := 0
 	for _, num := range target {
 		sum += num
 	}
-	for target[0] > 1 {
-		maxVal := target[0]
+
+	for max(target) > 1 {
+		maxVal := max(target)
 		restSum := sum - maxVal
+
 		if restSum <= 0 || restSum >= maxVal {
 			return false
 		}
+
 		newVal := maxVal % restSum
 		if newVal == 0 && restSum > 1 {
 			return false
 		}
+
 		sum = restSum + newVal
-		target[0] = newVal
-		sort.Sort(sort.Reverse(sort.IntSlice(target)))
+		updateMax(target, newVal)
 	}
 	return true
+}
+
+func max(arr []int) int {
+	maxVal := arr[0]
+	for _, num := range arr {
+		if num > maxVal {
+			maxVal = num
+		}
+	}
+	return maxVal
+}
+
+func updateMax(arr []int, newVal int) {
+	for i := range arr {
+		if arr[i] == max(arr) {
+			arr[i] = newVal
+			break
+		}
+	}
 }
