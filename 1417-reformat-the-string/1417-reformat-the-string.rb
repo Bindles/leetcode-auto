1599ec8592d4645b0242804c02062edc73f3eae1
Nicholas Turner <bindlez@gmail.com>
2024-08-18 12:24:37 -0400

    Time: 103 ms (20%), Space: 212 MB (20%) - LeetHub

--- 1417-reformat-the-string/1417-reformat-the-string.rb
+++ 1417-reformat-the-string/1417-reformat-the-string.rb
@@ -1,24 +1,18 @@
 # @param {String} s
 # @return {String}
 def reformat(s)
-  chars, digits = s.chars.partition { |c| c =~ /[a-zA-Z]/ }
-  
-  # Check if it's possible to interleave the characters
-  return '' if (chars.size - digits.size).abs > 1
-  
-  # If digits are more, swap chars and digits
-  chars, digits = digits, chars if digits.size > chars.size
-  
-  # Interleave characters and digits
-  result = []
-  while !chars.empty? && !digits.empty?
-    result << chars.shift
-    result << digits.shift
-  end
-
-  # Append remaining character if one list is longer
-  result << chars.shift unless chars.empty?
-  result << digits.shift unless digits.empty?
-
-  result.join
+    chars, digits = s.chars.partition { |c| c =~ /[a-zA-Z]/ }
+    s = ""
+    return s if ((chars.size > digits.size + 1) || (digits.size > chars.size + 1))
+    
+    (0..((chars.size > digits.size ? chars.size : digits.size) - 1)).each do |i|
+      if chars.size > digits.size
+        s << chars[i] unless chars[i].nil?
+        s << digits[i] unless digits[i].nil?
+      else
+        s << digits[i] unless digits[i].nil?
+        s << chars[i] unless chars[i].nil?
+      end
+    end
+    s
 end
