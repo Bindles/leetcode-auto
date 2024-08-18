42880aa160a79eb41fd13f7d4143cb84006c715c
Nicholas Turner <bindlez@gmail.com>
2024-08-18 12:59:00 -0400

    Time: 6 ms (74.7%), Space: 7.7 MB (35.54%) - LeetHub

--- 1362-closest-divisors/1362-closest-divisors.cpp
+++ 1362-closest-divisors/1362-closest-divisors.cpp
@@ -1,22 +1,10 @@
 class Solution {
 public:
-    vector<int> findDivisors(int target) {
-        for (int i = static_cast<int>(sqrt(target)); i > 0; --i) {
-            if (target % i == 0) {
-                return {i, target / i};
-            }
-        }
-        return {};
-    }
-
     vector<int> closestDivisors(int num) {
-        vector<int> div1 = findDivisors(num + 1);
-        vector<int> div2 = findDivisors(num + 2);
-
-        if (abs(div1[0] - div1[1]) < abs(div2[0] - div2[1])) {
-            return div1;
-        } else {
-            return div2;
+        for (int i = sqrt(num + 2); i >= 2; i--) {
+            if ((num + 1) % i == 0) return {i, (num + 1) / i};
+            else if ((num + 2) % i == 0) return {i, (num + 2) / i};
         }
+        return {1, num + 1};
     }
 };
