935cd284e9f892fb1fbd3e9636b54befc0802e3b
Nicholas Turner <bindlez@gmail.com>
2024-08-31 13:55:56 -0400

    Time: 83 ms (83.02%), Space: 212.8 MB (98.11%) - LeetHub

--- 0459-repeated-substring-pattern/0459-repeated-substring-pattern.rb
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.rb
@@ -1,8 +1,30 @@
 # @param {String} s
 # @return {Boolean}
+BASIC_PRIMES = [2, 3, 5]
 def repeated_substring_pattern(s)
-  for i in 1..(s.size / 2)
-    return true if s[0...i] * (s.size / i) == s
+  size = s.size
+  divisor = nil
+  
+  BASIC_PRIMES.each do |prime|
+    if size % prime == 0
+      divisor = prime
+      break
+    end
   end
+
+  divisor ||= 7
+
+  firstchar = s[0]
+  i = 1
+
+  while i <= size / divisor
+    si = s[i]
+    if size % i == 0 && si == firstchar
+      return true if s[0, i] * (size / i) == s
+    end
+
+    i += 1
+  end
+
   false
 end
