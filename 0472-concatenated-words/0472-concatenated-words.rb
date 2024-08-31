6f926f4c565495f75a5a6933cd663c5d6c9e48a1
Nicholas Turner <bindlez@gmail.com>
2024-08-31 14:47:32 -0400

    Time: 533 ms (25%), Space: 217.2 MB (20.83%) - LeetHub

--- 0472-concatenated-words/0472-concatenated-words.rb
+++ 0472-concatenated-words/0472-concatenated-words.rb
@@ -1,32 +1,19 @@
 # @param {String[]} words
 # @return {String[]}
 def find_all_concatenated_words_in_a_dict(words)
-  word_set = words.to_set
-  concatenated_words = []
-
-  words.each do |word|
-    word_set.delete(word)  # Remove current word to avoid using it in its own concatenation
-    if can_be_concatenated?(word, word_set)
-      concatenated_words << word
-    end
-    word_set.add(word)  # Add the word back to the set
-  end
-
-  concatenated_words
+    @m = words.each_with_object({'' => 0}) { |w, m| m[w] = 1 }
+    words.select { |w| check(w, 0) }
 end
 
-def can_be_concatenated?(word, word_set)
-  dp = Array.new(word.size + 1, false)
-  dp[0] = true
+def check(w, v, l = w.size)
+    return v >= 2 if l == 0
+    return false if @m[w] == -1
+    return true  if @m[w] && v + @m[w] >= 2 
 
-  (1..word.size).each do |i|
-    (0...i).each do |j|
-      if dp[j] && word_set.include?(word[j...i])
-        dp[i] = true
-        break
-      end
+    l.times do |i|
+      next if !@m[w[..i]] || @m[w[..i]] == -1
+      @m[w] = @m[w[(i + 1)..]] + 1 and return true if check(w[(i + 1)..], v + 1)
     end
-  end
-
-  dp[word.size] && dp[word.size]
+    @m[w] = -1 unless @m[w]
+    false
 end
