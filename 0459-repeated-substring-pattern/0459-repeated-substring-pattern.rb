b66b05f5829a99debbe2553ca5ab15d96ce8b4d1
Nicholas Turner <bindlez@gmail.com>
2024-08-31 14:01:48 -0400

    Time: 67 ms (94.34%), Space: 234.6 MB (52.83%) - LeetHub

--- 0459-repeated-substring-pattern/0459-repeated-substring-pattern.rb
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.rb
@@ -1,7 +1,7 @@
 # @param {String} s
 # @return {Boolean}
 def repeated_substring_pattern(s)
-    for i in 1..(s.size / 2)   
+   (1..(s.length / 2 )).each do |i|  
         if s.size % i == 0
             q = s.size / i
             s2 = s[0...i] * q
