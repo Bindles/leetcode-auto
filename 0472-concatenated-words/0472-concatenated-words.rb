a22ad3bb557fc2acb0ac88a6ce77725b026ca7c5
Nicholas Turner <bindlez@gmail.com>
2024-08-31 14:47:09 -0400

    Time: 833 ms (16.67%), Space: 216.9 MB (50%) - LeetHub

--- 0472-concatenated-words/0472-concatenated-words.rb
+++ 0472-concatenated-words/0472-concatenated-words.rb
@@ -1,5 +1,32 @@
 # @param {String[]} words
 # @return {String[]}
 def find_all_concatenated_words_in_a_dict(words)
-  (h=words.to_set).grep f = -> w,i=0{(i..w.size).any?{|j|h===w[i..j] and w[j+1] ? f[w,j+1] : i>0 } }    
+  word_set = words.to_set
+  concatenated_words = []
+
+  words.each do |word|
+    word_set.delete(word)  # Remove current word to avoid using it in its own concatenation
+    if can_be_concatenated?(word, word_set)
+      concatenated_words << word
+    end
+    word_set.add(word)  # Add the word back to the set
+  end
+
+  concatenated_words
+end
+
+def can_be_concatenated?(word, word_set)
+  dp = Array.new(word.size + 1, false)
+  dp[0] = true
+
+  (1..word.size).each do |i|
+    (0...i).each do |j|
+      if dp[j] && word_set.include?(word[j...i])
+        dp[i] = true
+        break
+      end
+    end
+  end
+
+  dp[word.size] && dp[word.size]
 end
