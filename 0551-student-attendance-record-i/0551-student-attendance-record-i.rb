68f3d24aeea2b0e65722355346250d7e4d443b7d
Nicholas Turner <bindlez@gmail.com>
2024-09-01 13:42:34 -0400

    Time: 96 ms (7.69%), Space: 211.7 MB (15.38%) - LeetHub

--- 0551-student-attendance-record-i/0551-student-attendance-record-i.rb
+++ 0551-student-attendance-record-i/0551-student-attendance-record-i.rb
@@ -1,15 +1,20 @@
 # @param {String} s
 # @return {Boolean}
 def check_record(s)
-    absences = consec_late = 0
+    absences = consec_late = max_late = 0
     s.chars.each_index do |i|
         case s[i]
         when "A"
             absences+=1
+            consec_late=0
         when "L"
-            s[i-1] == 'L' ? consec_late += 1 : consec_late = 1
+            s[i-1] == 'L' || nil ? consec_late += 1 : consec_late = 1
+            max_late = [max_late, consec_late].max         
         end
+        p "#{i} #{consec_late}"
     end
-    absences < 2 && consec_late < 3 ? true : false
+    p absences
+    p max_late
+    absences < 2 && max_late < 3 ? true : false
 
 end
