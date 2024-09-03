167afe93cf8c77bbe60cc1ded8cbca5e10f210f8
Nicholas Turner <bindlez@gmail.com>
2024-09-03 13:32:01 -0400

    Time: 86 ms (80%), Space: 212.3 MB (63.33%) - LeetHub

--- 0649-dota2-senate/0649-dota2-senate.rb
+++ 0649-dota2-senate/0649-dota2-senate.rb
@@ -1,25 +1,17 @@
 # @param {String} senate
 # @return {String}
 def predict_party_victory(senate)
-  r_queue = []
-  d_queue = []
+  r_queue, d_queue = [], []
+  n = senate.size
 
   senate.chars.each_with_index do |senator, i|
     senator == 'R' ? r_queue.push(i) : d_queue.push(i)
   end
 
-  n = senate.size
-
   while !r_queue.empty? && !d_queue.empty?
     r_index = r_queue.shift
     d_index = d_queue.shift
-
-    if r_index < d_index
-      r_queue.push(r_index + n)
-    else
-      d_queue.push(d_index + n)
-    end
+    r_index < d_index ? r_queue.push(r_index + n) : d_queue.push(d_index + n)
   end
-
   r_queue.empty? ? "Dire" : "Radiant"
 end
