a429f73a811ca31dcd880f4e64e49007e5a80060
Nicholas Turner <bindlez@gmail.com>
2024-09-04 16:01:19 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2462-total-cost-to-hire-k-workers/2462-total-cost-to-hire-k-workers.cpp
@@ -0,0 +1,46 @@
+struct Compare {
+    bool operator()(pair<int, int>& a, pair<int, int>& b) {
+        return a.second > b.second;
+    }
+};
+
+class Solution {
+public:
+    int totalCost(vector<int>& costs, int k, int candidates) {
+        priority_queue<pair<int, int>, vector<pair<int, int>>, Compare> pq;
+        int left = 0, right = costs.size() - 1;
+
+        for (int i = 0; i < candidates && left <= right; ++i) {
+            pq.push({left, costs[left]});
+            left++;
+            if (left <= right) {
+                pq.push({right, costs[right]});
+                right--;
+            }
+        }
+
+        int total_cost = 0;
+
+        while (k > 0) {
+            auto [index, cost] = pq.top();
+            pq.pop();
+            total_cost += cost;
+
+            if (index < left) {
+                if (left <= right) {
+                    pq.push({left, costs[left]});
+                    left++;
+                }
+            } else {
+                if (left <= right) {
+                    pq.push({right, costs[right]});
+                    right--;
+                }
+            }
+
+            k--;
+        }
+
+        return total_cost;
+    }
+};
