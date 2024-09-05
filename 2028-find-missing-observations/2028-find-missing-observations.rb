ff159bd8ed262b07a7bf66f9884443e98c1c51c7
Nicholas Turner <bindlez@gmail.com>
2024-09-05 17:42:17 -0400

    Time: 453 ms (100%), Space: 230.5 MB (100%) - LeetHub

--- 2028-find-missing-observations/2028-find-missing-observations.rb
+++ 2028-find-missing-observations/2028-find-missing-observations.rb
@@ -4,7 +4,7 @@
 # @return {Integer[]}
 def missing_rolls(rolls, mean, n)
   sum_needed = mean * (n + rolls.size) - rolls.sum
-  return [] if sum_needed < n || sum_needed > 6 * n
+  return [] if !sum_needed.between?(n, 6 * n)
 
   base, extra = sum_needed.divmod(n)
   Array.new(extra, base + 1) + Array.new(n - extra, base)
