48499246cd630e1af8c7a7f7e9ffe81c9b33e8fe
Nicholas Turner <bindlez@gmail.com>
2024-09-12 10:13:58 -0400

    Time: 233 ms (18.18%), Space: 211.5 MB (54.55%) - LeetHub

--- 3258-count-substrings-that-satisfy-k-constraint-i/3258-count-substrings-that-satisfy-k-constraint-i.rb
+++ 3258-count-substrings-that-satisfy-k-constraint-i/3258-count-substrings-that-satisfy-k-constraint-i.rb
@@ -2,12 +2,10 @@
 # @param {Integer} k
 # @return {Integer}
 def count_k_constraint_substrings(s, k)
-  total = 0
-  s.chars.each_index do |i|
-    (i...s.size).each do |j|
+  s.chars.each_index.sum do |i|
+    (i...s.size).count do |j|
       substring = s[i..j]
-      total += 1 if substring.count("1") <= k || substring.count("0") <= k
+      substring.count("1") <= k || substring.count("0") <= k
     end
   end
-  total
 end
