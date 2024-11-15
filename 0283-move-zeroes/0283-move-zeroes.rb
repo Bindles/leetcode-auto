3cc0097c52ac23af19223737a91350edd52fc21c
Nicholas Turner <bindlez@gmail.com>
2024-11-15 17:39:15 -0500

    Time: 7 ms (60.19%), Space: 212.4 MB (50.34%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.rb
+++ 0283-move-zeroes/0283-move-zeroes.rb
@@ -1,8 +1,19 @@
 # @param {Integer[]} nums
 # @return {Void} Do not return anything, modify nums in-place instead.
 def move_zeroes(nums)
-  old_len = nums.size
-  nums.delete(0)
-  zeroamount = Array.new(old_len-nums.size, 0)
-  nums.push(*zeroamount)
+  # Initialize a pointer for the non-zero position
+  non_zero_index = 0
+  
+  # Move all non-zero elements to the beginning of the array
+  nums.each do |num|
+    if num != 0
+      nums[non_zero_index] = num
+      non_zero_index += 1
+    end
+  end
+  
+  # Fill the remaining positions with zeros
+  (non_zero_index...nums.size).each do |i|
+    nums[i] = 0
+  end
 end
