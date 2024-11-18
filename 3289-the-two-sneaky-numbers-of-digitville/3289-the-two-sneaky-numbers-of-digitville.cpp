fd4595aae5330e4331e4827f5379851e01bbad6b
Nicholas Turner <bindlez@gmail.com>
2024-11-18 14:08:23 -0500

    Time: 4 ms (28.55%), Space: 27.3 MB (23.17%) - LeetHub

--- 3289-the-two-sneaky-numbers-of-digitville/3289-the-two-sneaky-numbers-of-digitville.cpp
+++ 3289-the-two-sneaky-numbers-of-digitville/3289-the-two-sneaky-numbers-of-digitville.cpp
@@ -3,13 +3,7 @@ public:
     vector<int> getSneakyNumbers(vector<int>& nums) {
         vector<int> res;
         unordered_map<int, int> countMap;
-        for (int num : nums) countMap[num]++;
-
-        // Debug: Print the countMap
-        cout << "CountMap:" << endl;
-        for (const auto& [num, count] : countMap) {
-            cout << "Number: " << num << ", Count: " << count << endl;
-        }   
+        for (int num : nums) countMap[num]++; 
 
         for (const auto& [num, count] : countMap) {
             if (count > 1) res.push_back(num);
