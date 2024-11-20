eeb44dd9b98fb1392d43064ba261c74d98f136ea
Nicholas Turner <bindlez@gmail.com>
2024-11-20 11:43:44 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0001-two-sum/0001-two-sum.rs
+++ 0001-two-sum/0001-two-sum.rs
@@ -1,11 +1,8 @@
 impl Solution {
     pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
-        let n = nums.len();
-        for i in 1..n {
-            for j in i..n {
-                if nums[j] + nums[j - i] == target {
-                    return vec![(j-i) as i32, j as i32]
-                }
+        for i in 0..=nums.len() { //i..nums.len() - 1|exclusive
+            if nums[i] + nums[i + 1] == target {
+                return vec![i as i32, (i+1) as i32]
             }
         }
         vec![-1,-1]
