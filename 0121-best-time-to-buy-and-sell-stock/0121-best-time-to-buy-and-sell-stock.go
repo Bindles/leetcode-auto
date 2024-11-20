925a385c6a998df6426598f60016113bf33ab59b
Nicholas Turner <bindlez@gmail.com>
2024-11-20 15:08:22 -0500

    Time: 0 ms (100%), Space: 9.9 MB (37.28%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.go
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.go
@@ -1,9 +1,12 @@
 func maxProfit(prices []int) int {
-  result := 0
-  lowest := prices[0]
-  for _, price := range prices {
-	lowest = min(lowest, price)
-	result = max(result, price-lowest)
-  }
-	return result    
+    maxprofit := 0
+    left := 0
+    for right := 1; right < len(prices); right++ {
+        if prices[left] > prices[right] {
+            left = right
+        } else {
+            maxprofit = max(maxprofit, prices[right]-prices[left])
+        }
+    }
+    return maxprofit
 }
