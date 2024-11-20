de463d01ca533dd2b09b72fd48c603194e131ba0
Nicholas Turner <bindlez@gmail.com>
2024-11-20 15:10:46 -0500

    Time: 57 ms (29.97%), Space: 221.2 MB (25.78%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.rb
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.rb
@@ -3,8 +3,12 @@
 def max_profit(prices)
   max_profit = 0
   left = 0
-  for right in 1...prices.size
-    prices[left] > prices[right] ? left = right : max_profit = [max_profit, prices[right] - prices[left]].max  
+  for right in 1...(prices.size)
+    if prices[left] > prices[right]
+      left = right
+    else
+      max_profit = [max_profit, prices[right] - prices[left]].max
+    end
   end
   max_profit
 end
