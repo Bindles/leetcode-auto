a398b284192c3bbbffc7b558bf8adad3f56b7306
Nicholas Turner <bindlez@gmail.com>
2024-11-20 11:32:29 -0500

    Time: 74 ms (25.51%), Space: 211.7 MB (93.58%) - LeetHub

--- 0001-two-sum/0001-two-sum.rb
+++ 0001-two-sum/0001-two-sum.rb
@@ -2,9 +2,12 @@
 # @param {Integer} target
 # @return {Integer[]}
 def two_sum(nums, target)
-  nums.each_index do |i|
-    (i+1...nums.length).each do |j|
-      return [i,j] if nums[i] + nums[j] == target
+  (1...nums.length).each do |i|
+    (i...nums.length).each do |j|
+      if nums[j] + nums[j - i] == target
+        return [j - i, j]
+      end
     end
   end
+  [-1, -1]
 end
