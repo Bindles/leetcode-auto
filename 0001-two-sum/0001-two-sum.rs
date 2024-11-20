fb2b0d015d980181a9973ee25da8d923997a2695
Nicholas Turner <bindlez@gmail.com>
2024-11-20 11:45:58 -0500

    Time: 0 ms (100%), Space: 2.5 MB (11.67%) - LeetHub

--- 0001-two-sum/0001-two-sum.rs
+++ 0001-two-sum/0001-two-sum.rs
@@ -1,9 +1,16 @@
+use std::collections::HashMap;
 impl Solution {
     pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
-        for i in 0..=nums.len() { //i..nums.len() - 1|exclusive
-            if nums[i] + nums[i + 1] == target {
-                return vec![i as i32, (i+1) as i32]
+        let mut map = HashMap::new();
+        for i in 0..nums.len() {
+            let num = nums[i];
+            let complement = target - num;
+
+            if map.contains_key(&complement) {
+                return vec![map[&complement] as i32, i as i32];
             }
+
+            map.insert(num,i);
         }
         vec![-1,-1]
     }
