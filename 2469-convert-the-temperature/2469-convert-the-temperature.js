56edf1240eaead5219c93c03d973645babff87e5
Nicholas Turner <bindlez@gmail.com>
2025-01-08 17:58:47 -0500

    Time: 0 ms (100%), Space: 48.6 MB (84.41%) - LeetHub

--- 2469-convert-the-temperature/2469-convert-the-temperature.js
+++ 2469-convert-the-temperature/2469-convert-the-temperature.js
@@ -2,8 +2,6 @@
  * @param {number} celsius
  * @return {number[]}
  */
-var convertTemperature = function(celsius) {
-  let kelvin = celsius + 273.15;
-  let fahrenheit = celsius * 1.80 + 32.00
-   return [kelvin, fahrenheit]
+var convertTemperature = function (celsius) {
+    return [celsius + 273.15, celsius * 1.80 + 32.00];
 };
