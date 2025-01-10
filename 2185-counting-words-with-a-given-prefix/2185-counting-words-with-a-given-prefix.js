9f3accf5b98f958bb232a2d36add73259f6c35f6
Nicholas Turner <bindlez@gmail.com>
2025-01-10 18:16:45 -0500

    Time: 0 ms (100%), Space: 50.7 MB (54.52%) - LeetHub

--- 2185-counting-words-with-a-given-prefix/2185-counting-words-with-a-given-prefix.js
+++ 2185-counting-words-with-a-given-prefix/2185-counting-words-with-a-given-prefix.js
@@ -4,9 +4,5 @@
  * @return {number}
  */
 var prefixCount = function(words, pref) {
-    let count=0;
-    for (word of words) {
-        if (word.startsWith(pref)) {count+=1;console.log(`${word} starts with ${pref}`)}
-    }
-    return count
+    return words.filter(word => word.startsWith(pref)).length
 };
