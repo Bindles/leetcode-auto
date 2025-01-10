f84e76a9e956423cfcb2eae08a3324db5ab4bbed
Nicholas Turner <bindlez@gmail.com>
2025-01-10 18:10:15 -0500

    Time: 0 ms (100%), Space: 13.5 MB (42.41%) - LeetHub

--- 2185-counting-words-with-a-given-prefix/2185-counting-words-with-a-given-prefix.cpp
+++ 2185-counting-words-with-a-given-prefix/2185-counting-words-with-a-given-prefix.cpp
@@ -1,9 +1,10 @@
 class Solution {
 public:
     int prefixCount(vector<string>& words, string& pref) {
-        return accumulate(words.begin(), words.end(), 0,
-        [pref](int sum, auto& w){
-            return sum+=w.starts_with(pref);
-        });
+        int count = 0;
+        for (string word : words) {
+            if (word.starts_with(pref)) count++;
+        }
+        return count;
     }
 };
