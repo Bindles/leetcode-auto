0438e3ab611c067526f1b06b745f7bca518944a6
Nicholas Turner <bindlez@gmail.com>
2025-01-12 22:54:26 -0500

    Time: 7 ms (67.23%), Space: 211.6 MB (57.06%) - LeetHub

--- 1408-string-matching-in-an-array/1408-string-matching-in-an-array.rb
+++ 1408-string-matching-in-an-array/1408-string-matching-in-an-array.rb
@@ -1,10 +1,7 @@
 # @param {String[]} words
 # @return {String[]}
 def string_matching(words)
-  w = words.sort_by(&:size)
-  result = []
-  w.each do |word|
-    result.push(word) if w.any? { |x| x != word && x.include?(word) }
+  words.each_with_object([]) do |word, result|
+    result << word if words.any? {|x| x != word && x.include?(word) }
   end
-  result
 end
