60d715b4248e8fd175b0ff720f40d0d4e98ef390
Nicholas Turner <bindlez@gmail.com>
2025-01-12 19:34:14 -0500

    Time: 0 ms (100%), Space: 2.2 MB (100%) - LeetHub

--- 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.rs
+++ 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.rs
@@ -4,10 +4,8 @@ impl Solution {
 
         for x in 1..s.len() {
             // Calculate the score for the current split
-            let left_score = s[0..x].chars().filter(|&c| c == '0').count();
-            let right_score = s[x..].chars().filter(|&c| c == '1').count();
-            let current_score = left_score + right_score;
-
+            let current_score = s[0..x].chars().filter(|&c| c == '0')
+            .count() + s[x..].chars().filter(|&c| c == '1').count();
             // Update the max score
             max_score = max_score.max(current_score);
         }
