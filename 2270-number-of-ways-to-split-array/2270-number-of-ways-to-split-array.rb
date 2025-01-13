bb28ad5d4edd6721fea22608971c8d57de9f7ec6
Nicholas Turner <bindlez@gmail.com>
2025-01-12 23:12:07 -0500

    Time: 67 ms (36.11%), Space: 222.6 MB (86.11%) - LeetHub

--- 2270-number-of-ways-to-split-array/2270-number-of-ways-to-split-array.rb
+++ 2270-number-of-ways-to-split-array/2270-number-of-ways-to-split-array.rb
@@ -4,8 +4,8 @@ def ways_to_split_array(nums)
   total_sum = nums.sum
   left_sum = 0
 
-  (0...nums.length - 1).select do |i|
+  (0...nums.length - 1).reduce(0) do |res, i|
     left_sum += nums[i]
-    left_sum >= (total_sum - left_sum)
-  end.size
+    res + (left_sum >= (total_sum - left_sum) ? 1 : 0)
+  end
 end
