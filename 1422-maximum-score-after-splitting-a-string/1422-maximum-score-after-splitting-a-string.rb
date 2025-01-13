fd8adb28d9f776f8ea4aa80670ce8938f1a209b0
Nicholas Turner <bindlez@gmail.com>
2025-01-12 19:25:52 -0500

    Time: 302 ms (8.7%), Space: 211.5 MB (68.32%) - LeetHub

--- 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.rb
+++ 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.rb
@@ -2,7 +2,7 @@
 # @return {Integer}
 def max_score(s)
     max_score=0
-    (1...s.size).each do |x|
+    (1...s.size).count do |x|
       max_score = [(s.chars[0...x].count("0") + s.chars[x...].count("1") ), max_score].max
     end
     max_score
