96ac769c14218735e384f24b5debea84ee09a0ac
Nicholas Turner <bindlez@gmail.com>
2025-01-12 23:22:44 -0500

    Time: 0 ms (100%), Space: 3.8 MB (26.88%) - LeetHub

--- 2270-number-of-ways-to-split-array/2270-number-of-ways-to-split-array.rs
+++ 2270-number-of-ways-to-split-array/2270-number-of-ways-to-split-array.rs
@@ -1,11 +1,11 @@
 impl Solution {
     pub fn ways_to_split_array(nums: Vec<i32>) -> i32 {
-        let total_sum: i32 = nums.iter().sum();
-        let mut left_sum: i32 = 0;
+        let total_sum: i64 = nums.iter().map(|&x| x as i64).sum();
+        let mut left_sum: i64 = 0;
         let mut res = 0;
 
         for i in 0..nums.len() - 1 {
-            left_sum += nums[i] as i32;
+            left_sum += nums[i] as i64;
             let right_sum = total_sum - left_sum;
 
             if left_sum >= right_sum {
