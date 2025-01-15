0da7465aedba2796135e6f2384e4eebab903864e
Nicholas Turner <bindlez@gmail.com>
2025-01-14 23:22:17 -0500

    Time: 3 ms (8.57%), Space: 2.4 MB (0%) - LeetHub

--- 2942-find-words-containing-character/2942-find-words-containing-character.rs
+++ 2942-find-words-containing-character/2942-find-words-containing-character.rs
@@ -1,8 +1,7 @@
 impl Solution {
     pub fn find_words_containing(words: Vec<String>, x: char) -> Vec<i32> {
         words.iter().enumerate()
-        .filter(|(_, w)| w.contains(x))
-        .map(|(index, _)| index as i32)
+        .filter_map(|(index, word)| if word.contains(x) {Some(index as i32)} else {None})
         .collect()
     }
 }
