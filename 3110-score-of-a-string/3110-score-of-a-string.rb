3f4bc3c873e415b694866a45ea66bc1a75616b13
Nicholas Turner <bindlez@gmail.com>
2025-01-14 22:23:59 -0500

    Time: 12 ms (5%), Space: 218.9 MB (0%) - LeetHub

--- 3110-score-of-a-string/3110-score-of-a-string.rb
+++ 3110-score-of-a-string/3110-score-of-a-string.rb
@@ -1,7 +1,7 @@
+# @param {String} s
+# @return {Integer}
 def score_of_string(s)
-  count=0 
-  (0...s.size-1).each do |i|
-    count += (s.bytes[i] - s.bytes[i+1]).abs
+  (0...s.size - 1).reduce(0) do |count, i|
+    count + (s.bytes[i] - s.bytes[i + 1]).abs
   end
-  count
 end
