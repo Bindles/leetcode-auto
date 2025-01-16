92c500355de7bc0b3054965be158faf344c42646
Nicholas Turner <bindlez@gmail.com>
2025-01-15 23:02:08 -0500

    Time: 0 ms (100%), Space: 2.3 MB (91.27%) - LeetHub

--- 3264-final-array-state-after-k-multiplication-operations-i/3264-final-array-state-after-k-multiplication-operations-i.rs
+++ 3264-final-array-state-after-k-multiplication-operations-i/3264-final-array-state-after-k-multiplication-operations-i.rs
@@ -1,21 +1,12 @@
 impl Solution {
     pub fn get_final_state(mut nums: Vec<i32>, k: i32, multiplier: i32) -> Vec<i32> {
         for _ in 0..k {
-            // Step 1: Find the smallest value
-            let mut min_value = nums[0];
-            let mut min_index = 0;
-
-            for (index, &value) in nums.iter().enumerate() {
-                if value < min_value {
-                    min_value = value;
-                    min_index = index;
-                }
+            // Find the smallest value and its index
+            if let Some((min_index, _)) = nums.iter().enumerate().min_by_key(|&(_, &val)| val) {
+                // Multiply the smallest value by the multiplier
+                nums[min_index] *= multiplier;
             }
-
-            // Step 2: Multiply the smallest value by the multiplier
-            nums[min_index] *= multiplier;
         }
-        // Return the updated array
         nums
     }
 }
