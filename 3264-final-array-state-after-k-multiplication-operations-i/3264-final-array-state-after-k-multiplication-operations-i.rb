5d7246abdf56a6e4b46834f4078d99fce612433f
Nicholas Turner <bindlez@gmail.com>
2025-01-15 23:15:26 -0500

    Time: 0 ms (100%), Space: 211.8 MB (85.4%) - LeetHub

--- 3264-final-array-state-after-k-multiplication-operations-i/3264-final-array-state-after-k-multiplication-operations-i.rb
+++ 3264-final-array-state-after-k-multiplication-operations-i/3264-final-array-state-after-k-multiplication-operations-i.rb
@@ -4,8 +4,7 @@
 # @param {Integer} multiplier
 # @return {Integer[]}
 def get_final_state(nums, k, multiplier)
-  k.times do 
+  k.times.each_with_object(nums) do |_,nums|
     nums[nums.index(nums.min)] *= multiplier
   end
-  nums
 end
