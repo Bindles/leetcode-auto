59fd0c54568c1ac150a62826137a70cc4b292ac9
Nicholas Turner <bindlez@gmail.com>
2025-01-15 23:00:58 -0500

    Time: 0 ms (100%), Space: 2.4 MB (32.84%) - LeetHub

--- 3264-final-array-state-after-k-multiplication-operations-i/3264-final-array-state-after-k-multiplication-operations-i.rs
+++ 3264-final-array-state-after-k-multiplication-operations-i/3264-final-array-state-after-k-multiplication-operations-i.rs
@@ -1,6 +1,7 @@
 impl Solution {
     pub fn get_final_state(mut nums: Vec<i32>, k: i32, multiplier: i32) -> Vec<i32> {
         for _ in 0..k {
+            // Step 1: Find the smallest value
             let mut min_value = nums[0];
             let mut min_index = 0;
 
@@ -11,8 +12,10 @@ impl Solution {
                 }
             }
 
+            // Step 2: Multiply the smallest value by the multiplier
             nums[min_index] *= multiplier;
         }
+        // Return the updated array
         nums
     }
 }
